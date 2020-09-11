9367c8c64cb424118c288b1d331c1f8dae9ae1d5
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-09-11 17:49:59 +0200

    update test

--- tests/add_image_and_scalar_test.cpp
+++ tests/add_image_and_scalar_test.cpp
@@ -22,7 +22,10 @@
 #include "clbuffer.h"
 #include "clgpu.h"
 
-#include "clmaximumzprojection.h"
+#include "claddimageandscalar.h"
+
+
+
 
 /**
  * Main test function
@@ -36,30 +39,14 @@ int main(int argc, char **argv)
     // Initialise random input and expected output.
     std::default_random_engine generator;
     std::normal_distribution<float> distribution(5.0,2.0);
-    unsigned int width (5), height (5), depth (5);
+    unsigned int width (5), height (5), depth (1);
     float* raw_data = new float[width*height*depth];
-    for (size_t d = 0; d < depth; d++)
+    float* res_data = new float[width*height*depth];
+    float scalar = 100.0f;
+    for (size_t i = 0; i < width*height*depth; i++)
     {
-        for (size_t y = 0; y < height; y++)
-        {
-            for (size_t x = 0; x < width; x++)
-            {
-                int i = x + width*(y+height*d);
-                if ( d == y )
-                {
-                    raw_data[i] = 100;
-                }
-                else
-                {
-                    raw_data[i] = distribution(generator);
-                }
-            }
-        }
-    }
-    float* res_data = new float[width*height*1];
-    for (size_t i = 0; i < width*height*1; i++)
-    {
-        res_data[i] = 100;
+        raw_data[i] = distribution(generator);
+        res_data[i] = raw_data[i] + scalar;
     }
 
     // Initialise device, context, and CQ.
@@ -69,26 +56,24 @@ int main(int argc, char **argv)
     // Push / Create input and output buffer
     Image<float> raw_image (raw_data, width, height, depth, "float");
     clBuffer gpuInput = gpu.Push<float>(raw_image);
-    std::array<unsigned int, 3> dimensions = {width, height, depth};
-    dimensions.back() = 1;
-    clBuffer gpuOutput = gpu.Create<float>(dimensions.data(), "float");
+    clBuffer gpuOutput = gpu.Create<float>(raw_image, "float");
 
     // Apply pipeline of kernels
-    clMaximumZProjection maximumProjection(gpu);
-    maximumProjection.Execute(gpuInput, gpuOutput);   
+    clAddImageAndScalar addImageAndScalarKernel(gpu);
+    addImageAndScalarKernel.Execute(gpuInput, gpuOutput, scalar);  
 
     // Pull output into container
     Image<float> out_image = gpu.Pull<float>(gpuOutput);    
 
     // Verify output
     float difference = 0;
-    for (size_t i = 0; i < width*height*1; i++)
+    for (size_t i = 0; i < width*height*depth; i++)
     {
         difference += std::abs(res_data[i] - out_image.GetData()[i]);
     }
     if (difference > std::numeric_limits<float>::epsilon())
     {
-        std::cout << difference << " > " << std::numeric_limits<float>::epsilon() << " , the test fail!" << std::endl;
+        std::cout << difference << " > " << std::numeric_limits<float>::epsilon() << std::endl;
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
