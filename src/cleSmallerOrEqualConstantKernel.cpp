c8c12bccaf4a245ea3386f7f2ed5ab05a91cdc0b
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-10-05 17:15:17 +0200

    update licence header

--- src/cleSmallerOrEqualConstantKernel.cpp
+++ src/cleSmallerOrEqualConstantKernel.cpp
@@ -1,6 +1,12 @@
-/**
- * Author: Stephane Rigaud - @strigaud 
- */
+/*  CLIc - version 0.1 - Copyright 2020 Stéphane Rigaud, Robert Haase,
+*   Institut Pasteur Paris, Max Planck Institute for Molecular Cell Biology and Genetics Dresden
+*
+*   CLIc is part of the clEsperanto project http://clesperanto.net 
+*
+*   This file is subject to the terms and conditions defined in
+*   file 'LICENSE.txt', which is part of this source code package.
+*/
+
 
 #include "cleSmallerOrEqualConstantKernel.h"
 
