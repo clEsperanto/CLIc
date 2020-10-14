ee1f1788dacb17442ded50d0ad0e0d334416f43f
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-10-14 14:49:45 +0200

    update top-files license comment

--- include/cleMean2DSphereKernel.h
+++ include/cleMean2DSphereKernel.h
@@ -1,7 +1,11 @@
-/**
- * Author: Robert Haase - @haesleinhuepf
- *         Stephane Rigaud - @strigaud 
- */
+/*  CLIc - version 0.1 - Copyright 2020 Stéphane Rigaud, Robert Haase,
+*   Institut Pasteur Paris, Max Planck Institute for Molecular Cell Biology and Genetics Dresden
+*
+*   CLIc is part of the clEsperanto project http://clesperanto.net 
+*
+*   This file is subject to the terms and conditions defined in
+*   file 'LICENSE.txt', which is part of this source code package.
+*/
 
 #ifndef __cleMean2DSphereKernel_h
 #define __cleMean2DSphereKernel_h
