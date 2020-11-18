d6bb98585cccaa697ba6b9dd6854ad92a11dd687
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-11-18 15:52:51 +0100

    generic function create/read/write buffer

--- include/utils.h
+++ include/utils.h
@@ -18,6 +18,7 @@
 #endif
 
 #include <string>
+#include <iostream>
 
 /**
  * save string into text file
@@ -28,5 +29,35 @@ void write_kernel_source(std::string);
  * OpenCL error code into string function
  */ 
 const char *getOpenCLErrorString(cl_int);
+
+
+cl_mem CreateBuffer(size_t, cl_context);
+
+template<class T>
+bool WriteBuffer(cl_mem mem_obj, T* arr, size_t bitSize, cl_command_queue command_queue)
+{
+    cl_int clError = clEnqueueWriteBuffer(command_queue, mem_obj, CL_TRUE, 0, bitSize, arr, 0, nullptr, nullptr);
+    if (clError != CL_SUCCESS)
+    {
+        std::cerr << "Push error! fail to write buffer in push() " << getOpenCLErrorString(clError) << std::endl;
+        throw clError;
+    }
+    return true;
+}
+
+template<class T>
+T* ReadBuffer(cl_mem mem_obj, size_t bitSize, cl_command_queue command_queue)
+{
+    size_t size = bitSize / sizeof(T);
+    T* arr = new T[size];
+    cl_int clError = clEnqueueReadBuffer(command_queue, mem_obj, CL_TRUE, 0, bitSize, arr, 0, NULL, NULL);
+    if (clError != CL_SUCCESS)
+    {
+        std::cerr << "Pull error! fail to read buffer : " << getOpenCLErrorString(clError) << std::endl;
+        throw clError;
+    }
+    return arr;
+}
+
 
 #endif // __utils_h
