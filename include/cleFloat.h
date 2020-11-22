3ca4e26fd5a8e2705c923cb758031fd82f6fb312
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-11-22 17:30:59 +0100

    add bitsize method

--- include/cleFloat.h
+++ include/cleFloat.h
@@ -38,6 +38,8 @@ public:
     std::string GetObjectType() const;
     std::string ToString() const;
     bool IsObject(std::string) const;
+    size_t GetBitSize() const;
+
 };
 
 } // namespace cle
