65a23488b58a18f7edd52e0095ba8f6eac985be7
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-11-25 13:31:22 +0100

    nonzeropixeltopixelindex kernel

---
+++ src/cleNonzeroMinimumBoxKernel.cpp
@@ -0,0 +1,50 @@
+/*  CLIc - version 0.1 - Copyright 2020 Stéphane Rigaud, Robert Haase,
+*   Institut Pasteur Paris, Max Planck Institute for Molecular Cell Biology and Genetics Dresden
+*
+*   CLIc is part of the clEsperanto project http://clesperanto.net 
+*
+*   This file is subject to the terms and conditions defined in
+*   file 'LICENSE.txt', which is part of this source code package.
+*/
+
+
+#include "cleNonzeroMinimumBoxKernel.h"
+
+namespace cle
+{
+
+void NonzeroMinimumBoxKernel::DefineDimensionality()
+{
+    std::string dim = "_2d";
+    Buffer* bufferObject = dynamic_cast<Buffer*>(parameterList.at("src"));
+    if(bufferObject->GetDimensions()[2] > 1)
+    {
+        dim = "_3d";
+    }
+    kernelName = kernelName + dim;
+}
+
+void NonzeroMinimumBoxKernel::SetInput(Object& x)
+{
+    this->AddObject(&x, "src");
+}
+
+void NonzeroMinimumBoxKernel::SetOutput1(Object& x)
+{
+    this->AddObject(&x, "dst");
+}
+
+void NonzeroMinimumBoxKernel::SetOutput2(Object& x)
+{
+    this->AddObject(&x, "flag_dst");
+}
+
+void NonzeroMinimumBoxKernel::Execute()
+{
+    DefineDimensionality();
+    CompileKernel();
+    AddArgumentsToKernel();
+    DefineRangeKernel();
+}
+
+} // namespace cle
