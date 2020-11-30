358839ac807a0fff68551c3c53c17530a3bc79aa
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-11-30 17:24:29 +0100

    add mean filter (test needed)

---
+++ src/cleMeanKernel.cpp
@@ -0,0 +1,47 @@
+/*  CLIc - version 0.1 - Copyright 2020 Stéphane Rigaud, Robert Haase,
+*   Institut Pasteur Paris, Max Planck Institute for Molecular Cell Biology and Genetics Dresden
+*
+*   CLIc is part of the clEsperanto project http://clesperanto.net 
+*
+*   This file is subject to the terms and conditions defined in
+*   file 'LICENSE.txt', which is part of this source code package.
+*/
+
+
+#include "cleMeanKernel.h"
+#include "cleExecuteSeparableKernel.h"
+
+namespace cle
+{
+
+void MeanKernel::SetInput(Object& x)
+{
+    this->AddObject(&x, "src");
+}
+
+void MeanKernel::SetOutput(Object& x)
+{
+    this->AddObject(&x, "dst");
+}
+
+void MeanKernel::SetSigma(float x, float y, float z)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+void MeanKernel::Execute()
+{
+    Buffer* src = dynamic_cast<Buffer*>(parameterList.at("src"));
+    Buffer* dst = dynamic_cast<Buffer*>(parameterList.at("dst"));
+
+    ExecuteSeparableKernel kernel(this->gpu);
+    kernel.SetKernelName(this->kernelName);
+    kernel.SetInput(*src);
+    kernel.SetOutput(*dst);
+    kernel.SetSigma(this->x, this->y, this->z);
+    kernel.Execute();
+}
+
+} // namespace cle
