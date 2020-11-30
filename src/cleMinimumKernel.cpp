3acfbbfed31eb2a165f381814d372a4b19806554
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2020-11-30 17:24:02 +0100

    add: minimum filter + test (to verify)

---
+++ src/cleMinimumKernel.cpp
@@ -0,0 +1,47 @@
+/*  CLIc - version 0.1 - Copyright 2020 Stéphane Rigaud, Robert Haase,
+*   Institut Pasteur Paris, Max Planck Institute for Molecular Cell Biology and Genetics Dresden
+*
+*   CLIc is part of the clEsperanto project http://clesperanto.net 
+*
+*   This file is subject to the terms and conditions defined in
+*   file 'LICENSE.txt', which is part of this source code package.
+*/
+
+
+#include "cleMinimumKernel.h"
+#include "cleExecuteSeparableKernel.h"
+
+namespace cle
+{
+
+void MinimumKernel::SetInput(Object& x)
+{
+    this->AddObject(&x, "src");
+}
+
+void MinimumKernel::SetOutput(Object& x)
+{
+    this->AddObject(&x, "dst");
+}
+
+void MinimumKernel::SetSigma(float x, float y, float z)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+void MinimumKernel::Execute()
+{
+    Buffer* src = dynamic_cast<Buffer*>(parameterList.at("src"));
+    Buffer* dst = dynamic_cast<Buffer*>(parameterList.at("dst"));
+
+    ExecuteSeparableKernel kernel(this->gpu);
+    kernel.SetKernelName(this->kernelName);
+    kernel.SetInput(*src);
+    kernel.SetOutput(*dst);
+    kernel.SetSigma(this->x, this->y, this->z);
+    kernel.Execute();
+}
+
+} // namespace cle
---
+++ test/minimum_test.cpp
@@ -0,0 +1,67 @@
+/*  CLIc - version 0.1 - Copyright 2020 Stéphane Rigaud, Robert Haase,
+*   Institut Pasteur Paris, Max Planck Institute for Molecular Cell Biology and Genetics Dresden
+*
+*   CLIc is part of the clEsperanto project http://clesperanto.net 
+*
+*   This file is subject to the terms and conditions defined in
+*   file 'LICENSE.txt', which is part of this source code package.
+*/
+
+#include <random>
+
+#include "CLE.h"
+
+/**
+ * Main test function
+ *
+ */
+int main(int argc, char **argv)
+{
+    // Initialise random input and valid output.
+    unsigned int width (3), height (3), depth (1);
+    float input_data[18] = {
+                1, 1, 1,
+                1, 0, 1,
+                1, 1, 1
+    };
+    float valid_data[18] = {
+                0, 0, 0,
+                0, 0, 0,
+                0, 0, 0
+    };
+    Image<float> input_img (input_data, width, height, depth, "float");
+
+    // Initialise GPU information.
+    cle::GPU gpu;
+    cle::CLE cle(gpu);
+
+    // Initialise device memory and push from host to device
+    cle::Buffer gpuInput = cle.Push<float>(input_img);
+    cle::Buffer gpuOutput = cle.Create<float>(gpuInput);
+
+    cle.Minimum(gpuInput, gpuOutput, 1.5, 1.5, 1.5);
+
+    // pull device memory to host
+    Image<float> output_img = cle.Pull<float>(gpuOutput);    
+
+    // Verify output
+    float difference = 0;
+    for (size_t i = 0; i < width*height*depth; i++)
+    {
+        if (i % width == 0) {
+            std::cout << std::endl;
+        }
+        std::cout << output_img.GetData()[i] << " ";
+        difference += std::abs(valid_data[i] - output_img.GetData()[i]);
+    }
+    std::cout << std::endl;
+    if (difference > std::numeric_limits<float>::epsilon())
+    {
+        std::cout << "Test failed, cumulated absolute difference " << difference << " > CPU epsilon (" << std::numeric_limits<float>::epsilon() << ")" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // That's all folks!
+    std::cout << "Test succeded!"<< std::endl;
+    return EXIT_SUCCESS;
+}
