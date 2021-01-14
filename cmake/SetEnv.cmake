d35ac3268664609f8f017ad990c1d65b34757b96
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-01-14 23:48:57 +0100

    coverage default off

--- cmake/SetEnv.cmake
+++ cmake/SetEnv.cmake
@@ -12,7 +12,6 @@ set(CMAKE_CXX_EXTENSIONS OFF) # Don't use e.g. GNU extension (like -std=gnu++11)
 option(BUILD_CODE_COVERAGE "Enable coverage reporting" OFF)
 if(BUILD_CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -g --coverage")
-  set(CMAKE_BUILD_TYPE Debug)
 endif()
 
 # Set PROJECT_NAME_UPPERCASE and PROJECT_NAME_LOWERCASE variables
