7317cb321f2aac740ce355983a0a36f9921243fe
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-01-26 13:15:52 +0100

    update Manager Class with clhpp opencl

--- clic/include/clePlatformManager.h
+++ clic/include/clePlatformManager.h
@@ -1,6 +1,18 @@
 #ifndef __clePlatformManager_h
 #define __clePlatformManager_h
 
+#ifndef CL_HPP_ENABLE_EXCEPTIONS
+#   define CL_HPP_ENABLE_EXCEPTIONS
+#endif
+
+#ifndef CL_HPP_TARGET_OPENCL_VERSION
+#   define CL_HPP_TARGET_OPENCL_VERSION 120
+#endif
+
+#ifndef CL_HPP_MINIMUM_OPENCL_VERSION
+#   define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#endif
+
 #ifndef CL_TARGET_OPENCL_VERSION
 #  define CL_TARGET_OPENCL_VERSION 120
 #endif
@@ -9,9 +21,10 @@
 #   include <OpenCL/opencl.h>
 #else
 #   include <CL/cl.h>
+#   include <CL/cl2.hpp>
 #endif
 
-#include <iostream>
+#include <vector>
 
 namespace cle
 {
@@ -19,17 +32,15 @@ namespace cle
 class PlatformManager
 {
 private:
-    cl_platform_id platform = nullptr;
+    std::vector<cl::Platform> m_PlatformList;
+    int m_PlatformId;
 
 public:
-    PlatformManager();
-    ~PlatformManager() = default;
+    PlatformManager(){};
+    PlatformManager(int);
+    ~PlatformManager();
 
-    void RequestPlatform();
-
-    cl_platform_id& GetPlatform();
-
-    std::string ToString() const;
+    cl::Platform GetPlatform(int=-1);
 };
 
 } // namespace cle
