6fe45d2512cd9c27317a9175d9907b4b564a0f30
App-Engine <20801457+4pp-Engine@users.noreply.github.com>
2021-01-29 16:08:24 +0100

    rework after core rework

--- benchmark/benchmark_maximum_projections.cpp
+++ benchmark/benchmark_maximum_projections.cpp
@@ -19,54 +19,22 @@ protected:
     cle::GPU gpu;
     cle::CLE cle;
     cle::Buffer gpuInput, gpuOutput;
-    float* input_data;
+    float* inputData;
 
     virtual void Setup()
     {
-        input_data = new float[dataWidth * dataWidth * dataWidth];
-        std::default_random_engine generator;
-        std::normal_distribution<float> distribution(5.0,2.0);
-        for (size_t d = 0; d < dataWidth; d++)
-        {
-            for (size_t y = 0; y < dataWidth; y++)
-            {
-                for (size_t x = 0; x < dataWidth; x++)
-                {
-                    int i = x + dataWidth*(y+dataWidth*d);
-                    if ( x == y )
-                    {
-                        input_data[i] = 1000;
-                    }
-                    else
-                    {
-                        input_data[i] = distribution(generator);
-                    }
-                }
-            }
-        }
-        Image<float> input_img (input_data, dataWidth, dataWidth, dataWidth, "float");
-
-        // Initialise GPU information.
-        cle::GPU gpu;
-        cle::CLE cle(gpu);
+        std::vector<float> inputData (dataWidth * dataWidth * dataWidth);
 
         // Initialise device memory and push from host
         std::array<unsigned int, 3> dimensions = {dataWidth, dataWidth, dataWidth};
-        dimensions.back() = 1;
-        gpuInput = cle.Push<float>(input_img);
-        gpuOutput = cle.Create<float>(dimensions.data(), "float");
+        gpuInput = cle.Push<float>(inputData, dimensions.data());
+        std::array<unsigned int, 3> dimensionsOutput = {dataWidth, dataWidth, 1};
+        gpuOutput = cle.Create<float>(dimensionsOutput.data());
     };
 
     virtual void Iteration() {};
 
-    virtual void Teardown()
-    {
-        delete[] input_data;
-    };
-
-    /* virtual void InterpretTiming(const string& title, const unsigned long ms) { */
-    /*     cout << title << ": " <<  ((float)(ms - 700)/(float)700 * 100) << "% wait inaccuracy" << endl; */
-    /* }; */
+    virtual void Teardown() {};
 
 public:
     unsigned dataWidth;
@@ -113,37 +81,44 @@ public:
     virtual ~MaximumZProjectionBenchmark(){}
 };
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        cout << "usage: " << argv[0] << " FILE" << endl;
+        return 1;
+    }
+
     MaximumXProjectionBenchmark x;
     MaximumYProjectionBenchmark y;
     MaximumZProjectionBenchmark z;
+
     ofstream csv;
-    csv.open("maximum_projection_benchmark.csv", std::ios_base::trunc);
-    csv << "width,count,x_compiletime_ms,x_runtime_ms,x_totaltime_ms,y_compiletime_ms,y_runtime_ms,y_totaltime_ms,z_compiletime_ms,z_runtime_ms,z_totaltime_ms" << std::endl;
+    csv.open(argv[1], std::ios_base::trunc);
+    if (!csv.is_open())
+    {
+        std::cerr << "Could not write to: " << argv[1] << std::endl;
+        return 1;
+    }
+    csv << "width,count,x_runtime_ms,y_runtime_ms,z_runtime_ms" << std::endl;
+
     for (int width = 1; width < 1024; width *= 2)
     {
         csv << width << "," << width*width*width << ",";
-        std::cout << "\n============\n dataWidth = " << width << "\n============\nMaximumXProjection: " << std::endl;
+        std::cout << "\n============\n dataWidth = " << width << "\n============";
+
+        std::cout << "\nMaximumXProjection: " << std::endl;
         x.dataWidth = width;
         x.Run();
-        unsigned long runtime = x.GetAvgNormalMs();
-        unsigned long total = x.GetAvgTotalMs();
-        static unsigned long xCompileTime = runtime;
-        csv << xCompileTime << "," << runtime << "," << total << ",";
+        csv << x.GetAvgNormalMs() << ",";
+
         std::cout << "\n\nMaximumYProjection: " << std::endl;
         y.dataWidth = width;
         y.Run();
-        runtime = y.GetAvgNormalMs();
-        total = y.GetAvgTotalMs();
-        static unsigned long yCompileTime = runtime;
-        csv << yCompileTime << "," << runtime << "," << total << ",";
+        csv << y.GetAvgNormalMs() << ",";
+
         std::cout << "\n\nMaximumZProjection: " << std::endl;
         z.dataWidth = width;
         z.Run();
-        runtime = z.GetAvgNormalMs();
-        total = z.GetAvgTotalMs();
-        static unsigned long zCompileTime = runtime;
-        csv << zCompileTime << "," << runtime << "," << total << std::endl;
+        csv << z.GetAvgNormalMs() << std::endl;
     }
     return 0;
 }
