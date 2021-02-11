b57f210e39d2c9027ed1a27c4eea42e635459e86
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-02-11 11:42:46 +0100

    add minimal comment

--- clic/tier0/cleKernel.h
+++ clic/tier0/cleKernel.h
@@ -19,7 +19,7 @@
 #  define CL_TARGET_OPENCL_VERSION 120
 #endif
 
-#   include <CL/cl2.hpp>
+#include <CL/cl2.hpp>
 
 #include <string>
 #include <fstream>
@@ -45,55 +45,51 @@ namespace cle
 class Kernel
 {
 private:
-
     cl::Program m_Program;
     cl::Kernel m_Kernel;
-    
-    // const std::string m_KernelFolder = "/mnt/data/workspace/C++/clEsperanto/CLIc_v2/clij-opencl-kernels/kernels";
-    // const std::string m_PreambleFile =  "/mnt/data/workspace/C++/clEsperanto/CLIc_v2/clic/preamble.cl";
 
-    const std::string m_KernelFolder = KERNELS_DIR; 
-    const std::string m_PreambleFile = PREAMBLE_OCL;
+    const std::string m_KernelFolder = KERNELS_DIR;  // ocl kernel folder path
+    const std::string m_PreambleFile = PREAMBLE_OCL; // ocl preamble file path
 
 protected:
-    // std::string m_Preamble;
-    // std::map<std::string, std::string> m_Sources;
-    std::string m_KernelName;
-    std::string m_DimensionTag = "";
-
     GPU m_gpu;
 
+    // kernel specifics
+    std::string m_KernelName;
+    std::string m_DimensionTag = "";
     std::vector<std::string> m_TagList;
     std::map<std::string, std::shared_ptr<cle::LightObject> > m_ParameterList;
 
+    // manage Execution range, and source has
     std::array<size_t,3> m_GlobalRange = {0, 0, 0};
     size_t m_CurrentHash = 0;
     bool m_BuildProgram = true;
 
-    std::string TypeAbbr(const std::string) const;
+    // Load/Generate OCL Source
     std::string LoadPreamble();
     std::string LoadSources();
     std::string LoadDefines();
     std::string GenerateSources();
+    std::string TypeAbbr(const std::string) const;
 
+    // Populate Parameter list with data and tag
     void AddObject(Buffer, std::string);
     void AddObject(int, std::string);
     void AddObject(float, std::string);
 
+    // Build, Set, and Run Kernel
+    void ManageDimensions(std::string);
     void BuildProgramKernel();
     void SetArguments();
     void EnqueueKernel();
-    void ManageDimensions(std::string);
 
 public:
     Kernel(GPU&, std::string, std::vector<std::string>);
     ~Kernel();
 
+    // virtual abstract method Execute
+    // Must be implemented in child kernel class
     virtual void Execute() = 0;
-    
-    std::string GetKernelName();
-    cl::Kernel GetKernel();
-    cl::Program GetProgram();
 };
 
 } // namespace cle
--- clic/tier1/cleAbsoluteKernel.cpp
+++ clic/tier1/cleAbsoluteKernel.cpp
@@ -7,7 +7,7 @@ namespace cle
 
 void AbsoluteKernel::SetInput(Buffer& x)
 {
-    this->AddObject(x, "src");
+    this->AddObject(x, "src"); // populate Parameter list with data and tag
 }
 
 void AbsoluteKernel::SetOutput(Buffer& x)
@@ -17,10 +17,10 @@ void AbsoluteKernel::SetOutput(Buffer& x)
 
 void AbsoluteKernel::Execute()
 {
-    this->ManageDimensions("dst");
-    this->BuildProgramKernel();
-    this->SetArguments();
-    this->EnqueueKernel();
+    this->ManageDimensions("dst");  // deal with 2d, 3d, nd
+    this->BuildProgramKernel();     // build program and kernel
+    this->SetArguments();           // add arguments to kernel
+    this->EnqueueKernel();          // enqueue kernel to GPU
 }
 
 } // namespace cle
