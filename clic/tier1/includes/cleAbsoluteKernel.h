6e5efa855a78658f28c7518c45fb26064bfa6203
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-07-06 16:37:36 +0200

    fix 2d and 3d kernel call

--- clic/tier1/includes/cleAbsoluteKernel.h
+++ clic/tier1/includes/cleAbsoluteKernel.h
@@ -11,7 +11,10 @@ class AbsoluteKernel : public Kernel
 {
 private:
     std::string source_1 = 
-        #include "cle_absolute.h" 
+        #include "cle_absolute_2d.h" 
+        ;
+    std::string source_2 = 
+        #include "cle_absolute_3d.h" 
         ;
 public:
     AbsoluteKernel(GPU& gpu) :    
@@ -20,7 +23,8 @@ public:
                 {"src" , "dst"} // parameter tags
         )
     {
-            m_Sources.insert({this->m_KernelName, source_1});
+            m_Sources.insert({this->m_KernelName + "_2d", source_1});
+            m_Sources.insert({this->m_KernelName + "_3d", source_2});
     }
 
     void SetInput(Buffer&);   // Set<Parameter>(<Type>)
