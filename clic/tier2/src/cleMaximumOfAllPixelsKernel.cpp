94705c693a7a482daa731f9861dd576b28019e83
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-07-06 17:54:26 +0200

    move kernel to T2

---
+++ clic/tier2/src/cleMaximumOfAllPixelsKernel.cpp
@@ -0,0 +1,60 @@
+
+#include "cleMaximumOfAllPixelsKernel.h"
+#include "cleMaximumZProjectionKernel.h"
+#include "cleMaximumYProjectionKernel.h"
+#include "cleMaximumXProjectionKernel.h"
+
+namespace cle
+{
+    
+void MaximumOfAllPixelsKernel::SetInput(Buffer& x)
+{
+    this->AddObject(x, "src");
+}
+
+void MaximumOfAllPixelsKernel::SetOutput(Buffer& x)
+{
+    this->AddObject(x, "dst_max");
+}
+
+void MaximumOfAllPixelsKernel::Execute()
+{
+    std::shared_ptr<Buffer> src = std::dynamic_pointer_cast<Buffer>(m_ParameterList.at("src"));
+    std::shared_ptr<Buffer> dst = std::dynamic_pointer_cast<Buffer>(m_ParameterList.at("dst_max"));
+    unsigned int dim[3] = {src->GetDimensions()[0], src->GetDimensions()[1], src->GetDimensions()[2]};
+
+    if (dim[2] > 1)
+    {
+        dim[2] = 1;
+        size_t size = dim[0] * dim[1] * dim[2];
+        cl::Buffer tmp1_obj = CreateBuffer<float>(size, this->m_gpu);
+        Buffer temp1 (tmp1_obj, dim, LightObject::Float);
+
+        MaximumZProjectionKernel kernelZ(this->m_gpu);
+        kernelZ.SetInput(*src);
+        kernelZ.SetOutput(temp1);
+        kernelZ.Execute();
+
+        *src = temp1;
+    }
+    if (dim[1] > 1)
+    {
+        dim[1] = 1;
+        size_t size = dim[0] * dim[1] * dim[2];
+        cl::Buffer tmp2_obj = CreateBuffer<float>(size, this->m_gpu);
+        Buffer temp2 (tmp2_obj, dim, LightObject::Float);
+
+        MaximumYProjectionKernel kernelY(this->m_gpu);
+        kernelY.SetInput(*src);
+        kernelY.SetOutput(temp2);
+        kernelY.Execute();
+
+        *src = temp2;
+    }
+    MaximumXProjectionKernel kernelX(this->m_gpu);
+    kernelX.SetInput(*src);
+    kernelX.SetOutput(*dst);
+    kernelX.Execute();
+}
+
+} // namespace cle
