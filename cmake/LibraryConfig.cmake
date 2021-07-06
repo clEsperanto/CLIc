bc29d4ceab3e288317d30e77cd21b87580191ac7
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-07-06 16:09:21 +0200

    update global cmake code

--- cmake/LibraryConfig.cmake
+++ cmake/LibraryConfig.cmake
@@ -3,32 +3,28 @@
 # https://github.com/pablospe/cmake-example-library
 #
 
-# OpenCL library
-find_package(OpenCL REQUIRED)
-if(OpenCL_FOUND)
-  include_directories(${OpenCL_INCLUDE_DIR})
-endif()
-
-# LibTiff library
-find_package(TIFF REQUIRED)
-if(TIFF_FOUND)
-  include_directories(${TIFF_INCLUDE_DIR})
+if(OpenCL_INCLUDE_DIR)
+  find_package(OpenCL REQUIRED)
+  if(OpenCL_FOUND)
+    include_directories(${OpenCL_INCLUDE_DIR}) 
+  endif()
 endif()
 
 # compile definition path to kernels and preamble .cl
 #   - (alt. option) replace by a generated header file ?
-add_compile_definitions(KERNELS_DIR="${CLI_KERNELS_DIR}")
-add_compile_definitions(PREAMBLE_OCL="${CLI_PREAMBLE_FILE}")
+# add_compile_definitions(KERNELS_DIR="${CLIC_KERNELS_DIR}")
+# add_compile_definitions(PREAMBLE_OCL="${CLIC_PREAMBLE_FILE}")
 
-set(HEADERS_PUBLIC ${HEADER_T0} ${HEADER_T1} ${HEADER_T2} ${HEADER_T3})
+set(PUBLIC_HEADERS ${HEADERS_core} ${HEADERS_T1})
 
 add_library(${LIBRARY_NAME}
-  ${SOURCES_T0} ${SOURCES_T1} ${SOURCES_T2} ${SOURCES_T3}
-  ${HEADER_T0} ${HEADER_T1} ${HEADER_T2} ${HEADER_T3}
-  )
+  ${SOURCES_core} ${HEADERS_core} 
+  ${SOURCES_T1} ${HEADERS_T1}
+  ${KERNELS_HEADERS}
+)
 
 # Target links
-target_link_libraries(${LIBRARY_NAME} OpenCL::OpenCL TIFF::TIFF)
+target_link_libraries(${LIBRARY_NAME} OpenCL::OpenCL)
 
 # Alias:
 #   - Foo::foo alias of foo
@@ -51,11 +47,9 @@ target_compile_definitions(${LIBRARY_NAME} PUBLIC
 #   - header location in project: ${CMAKE_CURRENT_BINARY_DIR}/generated_headers
 target_include_directories(
   ${LIBRARY_NAME} PUBLIC
-    "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/tier0>"
-    "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/tier1>"
-    "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/tier2>"
-    "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/tier3>"
-    "$<BUILD_INTERFACE:${GENERATED_HEADERS_DIR}>"
+    "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/kernels>"
+    "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/core/includes>"
+    "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/tier1/includes>"
     "$<INSTALL_INTERFACE:.>"
 )
 
@@ -75,29 +69,14 @@ install(
 # Headers:
 #   - foo/*.h -> <prefix>/include/*.h
 install(
-    FILES        ${HEADERS_PUBLIC}
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
+    FILES       ${PUBLIC_HEADERS}
+    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
-
-# Headers:
-#   - generated_headers/foo/version.h -> <prefix>/include/version.h
-install(
-    FILES       "${GENERATED_HEADERS_DIR}/${LIBRARY_FOLDER}/version.h"
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
-)
-
 
 # Kernels:
 #   - foo/*.cl -> <prefix>/include/kernel/*.cl
 install(
-    FILES        ${KERNELS}
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/kernels"
-)
-
-# Preamble:
-#   - foo/preamble.cl -> <prefix>/include/kernel/preamble.cl
-install(
-    FILES        ${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/preamble.cl
+    FILES        ${KERNELS_HEADERS}
     DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/kernels"
 )
 
--- cmake/SetEnv.cmake
+++ cmake/SetEnv.cmake
@@ -3,16 +3,12 @@
 # https://github.com/pablospe/cmake-example-library
 #
 
-# Use C++11
+
+## Project environement variables
+
 set(CMAKE_CXX_STANDARD 11) # Use C++11
 set(CMAKE_CXX_STANDARD_REQUIRED ON) # Require (at least) it
 set(CMAKE_CXX_EXTENSIONS OFF) # Don't use e.g. GNU extension (like -std=gnu++11) for portability
-
-# Set Coverage build flag
-option(BUILD_CODE_COVERAGE "Enable coverage reporting" OFF)
-if(BUILD_CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -g --coverage")
-endif()
 
 # Set PROJECT_NAME_UPPERCASE and PROJECT_NAME_LOWERCASE variables
 string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_UPPERCASE)
@@ -23,16 +19,39 @@ if(NOT LIBRARY_NAME)
   set(LIBRARY_NAME ${PROJECT_NAME})
 endif()
 
-# Library folder name (by default is the project name in lowercase)
-# Example: #include <foo/foo.h>
+# Library folder name (by default is the project name in lowercase), Example: #include <foo/foo.h>
 if(NOT LIBRARY_FOLDER)
   set(LIBRARY_FOLDER ${PROJECT_NAME_LOWERCASE})
 endif()
 
-# Make sure different configurations don't collide
-set(CMAKE_DEBUG_POSTFIX "d")
+# Set additional folder path
+set(PROJECT_THIRDPARTY_DIR "${PROJECT_SOURCE_DIR}/thirdparty" CACHE PATH "Third party libraries")
+set(PROJECT_UTILITIES_DIR "${PROJECT_SOURCE_DIR}/utilities" CACHE PATH "Utilities folder")
 
-# Select library type (default: STATIC)
+if(APPLE)
+  set(OpenCL_SUBDIR "OpenCL/")    
+else()
+  set(OpenCL_SUBDIR "CL/")    
+endif()
+
+# Set Preamble file 
+set(CLIC_PREAMBLE_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${LIBRARY_FOLDER}/preamble.cl" CACHE FILEPATH "CLIJ preamble file")
+mark_as_advanced(CLIC_PREAMBLE_FILE)
+
+# Set clij folder file 
+set(CLIC_KERNELS_DIR "${PROJECT_THIRDPARTY_DIR}/clij/kernels" CACHE PATH "CLIJ kernels directory")
+mark_as_advanced(CLIC_KERNELS_DIR)
+
+## Configuration and Build options
+
+# Set Code coverage options
+option(BUILD_CODE_COVERAGE "Enable coverage reporting" OFF)
+mark_as_advanced(BUILD_CODE_COVERAGE)
+if(BUILD_CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -g --coverage")
+endif()
+
+# Set library type options (default: STATIC)
 option(BUILD_SHARED_LIBS "Build ${LIBRARY_NAME} as a shared library." OFF)
 message(STATUS "BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 
@@ -40,57 +59,48 @@ message(STATUS "BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 option(BUILD_TESTING "Build ${LIBRARY_NAME} Tests." ON)
 message(STATUS "BUILD_TESTING: ${BUILD_TESTING}")
 
-# Set Kernel directory
-set(
-  CLI_KERNELS_DIR 
-  ${PROJECT_SOURCE_DIR}/clij-opencl-kernels/kernels 
-  CACHE PATH "CLI kernels directory"
-)
+# Set Doc compilation (default: OFF) (NOT IMPLEMENTED)
+option(BUILD_DOCUMENTATION "Build ${LIBRARY_NAME} Documentation." OFF)
+message(STATUS "BUILD_DOCUMENTATION: ${BUILD_DOCUMENTATION} (NOT IMPLEMENTED)")
 
-# Set Preamcle file 
-set(
-  CLI_PREAMBLE_FILE 
-  ${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/preamble.cl 
-  CACHE FILEPATH "CLI preamble file"
-)
+# Set OpenCL Standard version number (default: 120)
+set(OPENCL_VERSION 120)
+mark_as_advanced(OPENCL_VERSION)
+message(STATUS "OPENCL_VERSION: ${OPENCL_VERSION}")
 
-# Build type (default: RELEASE)
-#
-# No reason to set CMAKE_CONFIGURATION_TYPES if it's not a multiconfig generator
-# Also no reason of using CMAKE_BUILD_TYPE if it's a multiconfig generator.
-#
+# Manage build type options (default: RELEASE)
 get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 if(isMultiConfig)
-  set(CMAKE_CONFIGURATION_TYPES
-      "Release;Debug;MinSizeRel;RelWithDebInfo" CACHE STRING "" FORCE)
-
+  set(CMAKE_CONFIGURATION_TYPES "Release;Debug;MinSizeRel;RelWithDebInfo" CACHE STRING "" FORCE)
   message(STATUS "CMAKE_CONFIGURATION_TYPES: ${CMAKE_CONFIGURATION_TYPES}")
   message(STATUS "CMAKE_GENERATOR: Multi-config")
 else()
-  # Set a default build type if none was specified
   if(NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
   endif()
-
-  # Set the possible values of build type for cmake-gui
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
-               "Release" "Debug" "MinSizeRel" "RelWithDebInfo")
-
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Release" "Debug" "MinSizeRel" "RelWithDebInfo")
   message(STATUS "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}")
   message(STATUS "CMAKE_GENERATOR: Single-config")
 endif()
 message(STATUS "CMAKE_GENERATOR: ${CMAKE_GENERATOR}")
 
-# Generated headers folder
-set(GENERATED_HEADERS_DIR
-  "${CMAKE_CURRENT_BINARY_DIR}/generated_headers"
-)
+# Configurations tag to avoid compilation colliding
+set(CMAKE_DEBUG_POSTFIX "d")
+set(CMAKE_RELEASE_POSTFIX "")
 
-# Create 'version.h'
+
+
+
+## Install and Uninstall configuration
+
+# Create and configure 'clic.h'
+#   - define OpenCL version
+#   - include OpenCL headers
+#   - define CLIC version
 configure_file(
-  "${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/version.h.in"
-  "${GENERATED_HEADERS_DIR}/${LIBRARY_FOLDER}/version.h"
-  @ONLY
+  "${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/${PROJECT_NAME_LOWERCASE}.h.in"
+  "${PROJECT_SOURCE_DIR}/${LIBRARY_FOLDER}/core/includes/${PROJECT_NAME_LOWERCASE}.h"
+  NO_SOURCE_PERMISSIONS @ONLY
 )
 
 # Introduce variables:
@@ -98,6 +108,11 @@ configure_file(
 #   - CMAKE_INSTALL_BINDIR
 #   - CMAKE_INSTALL_INCLUDEDIR
 include(GNUInstallDirs)
+
+# Include module with functions:
+#   - write_basic_package_version_file(...)
+#   - configure_package_config_file(...)
+include(CMakePackageConfigHelpers)
 
 # Layout. This works for all platforms:
 #   - <prefix>/lib*/cmake/<PROJECT-NAME>
@@ -111,18 +126,11 @@ set(VERSION_CONFIG_FILE "${GENERATED_DIR}/${PROJECT_NAME}ConfigVersion.cmake")
 set(PROJECT_CONFIG_FILE "${GENERATED_DIR}/${PROJECT_NAME}Config.cmake")
 set(TARGETS_EXPORT_NAME "${PROJECT_NAME}Targets")
 
-# Include module with functions:
-#   - write_basic_package_version_file(...)
-#   - configure_package_config_file(...)
-include(CMakePackageConfigHelpers)
-
 # Configure '<PROJECT-NAME>ConfigVersion.cmake'
 # Use:
 #   - PROJECT_VERSION
-write_basic_package_version_file(
-    "${VERSION_CONFIG_FILE}"
-    VERSION "${${PROJECT_NAME}_VERSION}"
-    COMPATIBILITY SameMajorVersion
+write_basic_package_version_file( 
+  "${VERSION_CONFIG_FILE}" VERSION "${${PROJECT_NAME}_VERSION}" COMPATIBILITY SameMajorVersion
 )
 
 # Configure '<PROJECT-NAME>Config.cmake'
@@ -130,21 +138,23 @@ write_basic_package_version_file(
 #   - TARGETS_EXPORT_NAME
 #   - PROJECT_NAME
 configure_package_config_file(
-    "${PROJECT_SOURCE_DIR}/cmake/Config.cmake.in"
-    "${PROJECT_CONFIG_FILE}"
-      INSTALL_DESTINATION "${CONFIG_INSTALL_DIR}"
-      PATH_VARS CMAKE_INSTALL_PREFIX CMAKE_INSTALL_LIBDIR CMAKE_INSTALL_INCLUDEDIR 
+    "${PROJECT_SOURCE_DIR}/cmake/Config.cmake.in" "${PROJECT_CONFIG_FILE}"
+    INSTALL_DESTINATION "${CONFIG_INSTALL_DIR}"
+    PATH_VARS CMAKE_INSTALL_PREFIX CMAKE_INSTALL_LIBDIR CMAKE_INSTALL_INCLUDEDIR 
 )
 
 # Uninstall targets
-configure_file("${PROJECT_SOURCE_DIR}/cmake/Uninstall.cmake.in"
-  "${GENERATED_DIR}/Uninstall.cmake"
-  IMMEDIATE @ONLY)
-add_custom_target(uninstall
-  COMMAND ${CMAKE_COMMAND} -P ${GENERATED_DIR}/Uninstall.cmake)
+configure_file("${PROJECT_SOURCE_DIR}/cmake/Uninstall.cmake.in" "${GENERATED_DIR}/Uninstall.cmake"
+  IMMEDIATE @ONLY
+)
+add_custom_target(uninstall COMMAND ${CMAKE_COMMAND} -P ${GENERATED_DIR}/Uninstall.cmake)
+
+
+
+## Other?? 
 
 # Always full RPATH (for shared libraries)
-#  https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/RPATH-handling
+# https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/RPATH-handling
 if(BUILD_SHARED_LIBS)
   # use, i.e. don't skip the full RPATH for the build tree
   set(CMAKE_SKIP_BUILD_RPATH FALSE)
@@ -152,7 +162,6 @@ if(BUILD_SHARED_LIBS)
   # when building, don't use the install RPATH already
   # (but later on when installing)
   set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
-
   set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
 
   # add the automatically determined parts of the RPATH
@@ -165,6 +174,7 @@ if(BUILD_SHARED_LIBS)
       set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
   endif()
 endif()
+
 
 # CMake Registry
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/CMakeRegistry.cmake)
