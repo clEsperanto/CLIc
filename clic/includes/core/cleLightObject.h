ea861138e65602af08870154cfdbed15943ced30
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-07-15 16:57:56 +0200

    clean bitsize method

--- clic/includes/core/cleLightObject.h
+++ clic/includes/core/cleLightObject.h
@@ -21,7 +21,6 @@ public:
 
     virtual std::string GetObjectType() const = 0;
     virtual bool IsObject(ObjectType) const = 0;
-    virtual size_t GetBitSize() const = 0;
 
 protected:    
     std::string ObjectTypeToString(const ObjectType) const;
