57d5a16d30d87a5783dc439e689b035e8182750d
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-07-19 17:15:59 +0200

    cleKernelList autogeneration

---
+++ utilities/autogeneration_script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys, os
+
+def get_includes_list(folder):
+    includes_list = []
+    for root, _, files in os.walk(folder):
+        if root.split(os.sep)[-1] != "core": 
+            for file in files:
+                    includes_list.append(file)
+    return sorted(includes_list)
+
+
+def generate_kernel_list_header(folder):
+    kernel_list_header_file = os.path.join(folder,os.path.join("core","cleKernelList.h"))
+    include_list = get_includes_list(folder)
+    with open(kernel_list_header_file, 'w') as output_file:
+        output_file.write("#ifndef __cleKernelList_h\n")
+        output_file.write("#define __cleKernelList_h\n")
+        output_file.write("\n")
+        for include in include_list:
+            output_file.write("#include \"{0}\"\n".format(include))
+        output_file.write("\n")
+        output_file.write("#endif //__cleKernelList_h\n")
+
+generate_kernel_list_header(sys.argv[1])
