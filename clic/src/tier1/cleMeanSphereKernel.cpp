cf1ededfdd02ff834b5167143689050a74713c25
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-07-19 14:29:51 +0200

    merge SetRadiusX,Y,Z() into one SetRadius()

--- clic/src/tier1/cleMeanSphereKernel.cpp
+++ clic/src/tier1/cleMeanSphereKernel.cpp
@@ -9,6 +9,19 @@ int MeanSphereKernel::Radius2KernelSize(float r)
     return int(r) * 2 + 1;
 }
 
+void MeanSphereKernel::SetRadius(int x, int y, int z)
+{
+    int dx = Radius2KernelSize(x);
+    this->AddObject(dx, "radius_x");
+    int dy = Radius2KernelSize(y);
+    this->AddObject(dy, "radius_y");
+    if(z > 0)
+    {
+        int dz = Radius2KernelSize(z);
+        this->AddObject(dz, "radius_z");
+    }
+}
+
 void MeanSphereKernel::SetInput(Buffer& x)
 {
     this->AddObject(x, "src");
@@ -17,24 +30,6 @@ void MeanSphereKernel::SetInput(Buffer& x)
 void MeanSphereKernel::SetOutput(Buffer& x)
 {
     this->AddObject(x, "dst");
-}
-
-void MeanSphereKernel::SetRadiusX(int x)
-{
-    int s = Radius2KernelSize(x);
-    this->AddObject(s, "radius_x");
-}
-
-void MeanSphereKernel::SetRadiusY(int x)
-{
-    int s = Radius2KernelSize(x);
-    this->AddObject(s, "radius_y");
-}
-
-void MeanSphereKernel::SetRadiusZ(int x)
-{
-    int s = Radius2KernelSize(x);
-    this->AddObject(s, "radius_z");
 }
 
 void MeanSphereKernel::Execute()
