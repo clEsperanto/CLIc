be091533ed56e1afe9e65072bc3b12645fae8739
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-08-25 16:47:13 +0200

    fix test

--- clic/tests/scalar_test.cpp
+++ clic/tests/scalar_test.cpp
@@ -22,7 +22,7 @@ int scalar_test(const char* c_type)
     res += my_scalar->IsDataType("toto") != 0 ;
     res += strncmp(my_scalar->GetObjectType(), "buffer", strlen("buffer")) != 0 ;
     res += strncmp(my_scalar->GetObjectType(), "scalar", strlen("scalar")) == 0 ;
-    res += !(strncmp(my_scalar->GetDataType(), c_type, strlen(c_type)) == 0) ;
+    res += strncmp(my_scalar->GetDataType(), c_type, strlen(c_type)) == 0 ;
     res += strncmp(my_scalar->GetDataType(), "toto", strlen("toto")) != 0 ;
     res += my_scalar->GetObject() == value;
     delete my_scalar;
