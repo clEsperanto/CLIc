17b79a11c7b56381741990876e644cd38b769e2e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-08-30 16:27:51 +0200

    create Image2D class (not working)

---
+++ clic/src/core/cleImage2D.cpp
@@ -0,0 +1,119 @@
+#include "cleImage2D.hpp"
+
+namespace cle
+{
+Image2D::Image2D()
+{}
+
+Image2D::Image2D(cl::Image2D _ocl, DataType _t) : m_Object(_ocl), m_Type(_t)
+{}
+
+Image2D::Image2D(cl::Image2D _ocl, std::array<int,3> _dim, DataType _t) : m_Object(_ocl), m_Type(_t)
+{
+    std::copy(_dim.begin(), _dim.end(), m_Shape.begin());
+    std::copy(_dim.begin(), _dim.end(), m_Region.begin());
+    m_ImageFormat.image_channel_order = _ocl.getImageInfo<CL_IMAGE_FORMAT>().image_channel_order;          
+    m_ImageFormat.image_channel_data_type = _ocl.getImageInfo<CL_IMAGE_FORMAT>().image_channel_data_type;  
+}
+
+Image2D::Image2D(cl::Image2D _ocl, int _dim[3] , DataType _t) : m_Object(_ocl), m_Type(_t)
+{
+    std::copy(_dim, _dim+3, m_Shape.begin());
+    std::copy(_dim, _dim+3, m_Region.begin());
+    m_ImageFormat.image_channel_order = _ocl.getImageInfo<CL_IMAGE_FORMAT>().image_channel_order;          
+    m_ImageFormat.image_channel_data_type = _ocl.getImageInfo<CL_IMAGE_FORMAT>().image_channel_data_type;  
+}
+
+cl::Image2D Image2D::GetObject() const
+{
+    return this->m_Object; 
+}
+
+int Image2D::GetWidth() const
+{
+    return m_Shape[0];
+}
+
+int Image2D::GetHeight() const
+{
+    return m_Shape[1];
+}
+
+int Image2D::GetDepth() const
+{
+    return m_Shape[2];
+}
+
+int Image2D::GetSize() const
+{
+    return m_Shape[0]*m_Shape[1]*m_Shape[2];
+}
+
+int Image2D::GetDimension() const
+{
+    return 2;
+}
+
+std::array<int,3> Image2D::GetShape() const
+{
+    return m_Shape;
+}
+
+std::array<int,3> Image2D::GetOrigin() const
+{
+    return m_Origin;
+}
+
+std::array<int,3> Image2D::GetRegion() const
+{
+    return m_Region;
+}
+
+void Image2D::SetOrigin(std::array<int,3> _origin)
+{
+    std::copy(_origin.begin(), _origin.end(), m_Origin.begin());    
+}
+
+void Image2D::SetRegion(std::array<int,3> _region)
+{
+    std::copy(_region.begin(), _region.end(), m_Region.begin());    
+}
+
+const char* Image2D::GetDataType() const
+{
+    if (m_Type == DataType::FLOAT) return "float"; 
+    if (m_Type == DataType::DOUBLE) return "double"; 
+    if (m_Type == DataType::INT) return "int"; 
+    if (m_Type == DataType::CHAR) return "char"; 
+    if (m_Type == DataType::UINT) return "uint"; 
+    if (m_Type == DataType::UCHAR) return "uchar"; 
+    if (m_Type == DataType::SHORT) return "short"; 
+    if (m_Type == DataType::USHORT) return "ushort"; 
+    return "";
+}
+
+bool Image2D::IsDataType(const char* str) const
+{
+    size_t size = strlen(str);
+    if (m_Type == DataType::FLOAT && strncmp("float", str, size) == 0) return true; 
+    if (m_Type == DataType::DOUBLE && strncmp("double", str, size) == 0) return true; 
+    if (m_Type == DataType::INT && strncmp("int", str, size) == 0) return true; 
+    if (m_Type == DataType::CHAR && strncmp("char", str, size) == 0) return true;
+    if (m_Type == DataType::UINT && strncmp("uint", str, size) == 0) return true; 
+    if (m_Type == DataType::UCHAR && strncmp("uchar", str, size) == 0) return true; 
+    if (m_Type == DataType::SHORT && strncmp("short", str, size) == 0) return true; 
+    if (m_Type == DataType::USHORT && strncmp("ushort", str, size) == 0) return true; 
+    return false;
+}   
+
+const char* Image2D::GetObjectType() const
+{
+    return "image2dimage2d";
+}
+
+bool Image2D::IsObjectType(const char* str) const
+{
+    return strncmp("image2d", str, strlen(str)) == 0;
+}
+
+}
---
+++ clic/tests/image_test.cpp
@@ -0,0 +1,123 @@
+
+#include "clesperanto.hpp"
+
+#include "cleAddImageAndScalarKernel.hpp"
+
+int main(int argc, char** argv)
+{
+    cle::Clesperanto* cle = new cle::Clesperanto();
+
+    {
+    int dims[3] = {16, 1, 1};
+    std::vector<float> A (dims[0]*dims[1]*dims[2]); 
+    std::vector<float> B (dims[0]*dims[1]*dims[2]); 
+
+    std::fill (A.begin(),A.end(), 10.0f);
+    std::fill (B.begin(),B.end(), 1.0f);
+
+
+    cl::Image1D image = cle::CreateImage1D<float>(dims, cle->GetGPU());
+    cle::WriteImage1D<float>(image, A.data(), cle->GetGPU());
+    cle::ReadImage1D<float>(image, B.data(), cle->GetGPU());
+
+    float diff = 0;
+    for (size_t i=0; i<A.size(); i++)
+    {
+        diff += A[i] - B[i];
+    }
+    std::cout << diff << std::endl;
+    }
+
+    {
+    int dims[3] = {16, 16, 1};
+    std::vector<float> A (dims[0]*dims[1]*dims[2]); 
+    std::vector<float> B (dims[0]*dims[1]*dims[2]); 
+
+    std::fill (A.begin(),A.end(), 10.0f);
+    std::fill (B.begin(),B.end(), 1.0f);
+
+
+    cl::Image2D image = cle::CreateImage2D<float>(dims, cle->GetGPU());
+    cle::WriteImage2D<float>(image, A.data(), cle->GetGPU());
+    cle::ReadImage2D<float>(image, B.data(), cle->GetGPU());
+
+    float diff = 0;
+    for (size_t i=0; i<A.size(); i++)
+    {
+        diff += A[i] - B[i];
+    }
+    std::cout << diff << std::endl;
+    }
+
+    {
+    int dims[3] = {16, 16, 5};
+    std::vector<float> A (dims[0]*dims[1]*dims[2]); 
+    std::vector<float> B (dims[0]*dims[1]*dims[2]); 
+
+    std::fill (A.begin(),A.end(), 10.0f);
+    std::fill (B.begin(),B.end(), 1.0f);
+
+
+    cl::Image3D image = cle::CreateImage3D<float>(dims, cle->GetGPU());
+    cle::WriteImage3D<float>(image, A.data(), cle->GetGPU());
+    cle::ReadImage3D<float>(image, B.data(), cle->GetGPU());
+
+    float diff = 0;
+    for (size_t i=0; i<A.size(); i++)
+    {
+        diff += A[i] - B[i];
+    }
+    std::cout << diff << std::endl;
+    }
+
+
+
+    {
+    int dims[3] = {16, 16, 1};
+    std::vector<float> A (dims[0]*dims[1]*dims[2]); 
+    std::vector<float> B (dims[0]*dims[1]*dims[2]); 
+    std::vector<float> C (dims[0]*dims[1]*dims[2]); 
+
+    std::fill (A.begin(),A.end(), 1.0f);
+    std::fill (B.begin(),B.end(), 0.0f);
+    std::fill (C.begin(),C.end(), 101.0f);
+
+    cl::Image2D ocl_imageA = cle::CreateImage2D<float>(dims, cle->GetGPU());
+    cle::WriteImage2D<float>(ocl_imageA, A.data(), cle->GetGPU());
+    cle::Image2D ImageA (ocl_imageA, dims);
+
+    cl::Image2D ocl_imageB = cle::CreateImage2D<float>(dims, cle->GetGPU());
+    cle::Image2D ImageB (ocl_imageB, dims);
+
+
+    // cl::Buffer ocl_imageA = cle::CreateBuffer<float>(A.size(), cle->GetGPU());
+    // cle::WriteBuffer<float>(ocl_imageA, A.data(), A.size(), cle->GetGPU());
+    // cle::Buffer ImageA (ocl_imageA, dims);
+
+    // cl::Buffer ocl_imageB = cle::CreateBuffer<float>(B.size(), cle->GetGPU());
+    // cle::Buffer ImageB (ocl_imageB, dims);
+
+    cle::AddImageAndScalarKernel kernel (cle->GetGPU());
+    kernel.SetScalar(100.0f);
+    kernel.SetInput(ImageA);
+    kernel.SetOutput(ImageB);
+    kernel.Execute();
+
+    cle::ReadImage2D<float>(ImageB.GetObject(), B.data(), cle->GetGPU());
+    // cle::ReadBuffer<float>(ImageB.GetObject(), B.data(), ImageB.GetSize(), cle->GetGPU());
+
+    float diff = 0;
+    for (size_t i=0; i<A.size(); i++)
+    {
+        diff += B[i] - C[i];
+    }
+    std::cout << diff << std::endl;
+    }
+
+
+
+
+
+
+    return 0;
+}
