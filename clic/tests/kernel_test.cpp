383d46f5bdc0afd3e51ac5a5cbc322508d9d3e2f
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2021-11-22 17:25:07 +0100

    ENH: extend kernel unity test for i,ui,c,uc,s,us

--- clic/tests/kernel_test.cpp
+++ clic/tests/kernel_test.cpp
@@ -202,6 +202,239 @@ int main(int argc, char **argv)
     }
     }
 
+    {
+    using TYPE = int;
+    auto gpu = std::make_shared<cle::GPU>();
+    gpu->WaitForKernelToFinish();
+
+    std::array<int,3> dims = {10, 1, 1};
+    std::vector<TYPE> data (dims[0]*dims[1]*dims[2]); 
+    std::vector<TYPE> valid (dims[0]*dims[1]*dims[2]); 
+    std::fill (data.begin(),data.end(), 10.0f);
+    std::fill (valid.begin(),valid.end(), 100.0f);
+
+    cle::Buffer buff_A = gpu->PushBuffer<TYPE>(data, dims);
+    cle::Buffer buff_B = gpu->CreateBuffer<TYPE>(dims);
+
+    cle::AddImageAndScalarKernel kernel(gpu);
+    kernel.SetInput(buff_A);
+    kernel.SetOutput(buff_B);
+    kernel.SetScalar(90.0f);
+    kernel.Execute();
+
+    std::vector<TYPE> res = gpu->Pull<TYPE>(buff_B);
+    
+
+    float diff = 0;
+    for (int i =0; i< res.size(); i++)
+    {
+        diff += valid[i] - res[i];
+    }
+    if (diff > 0)
+    {
+        std::cout << "test kernel fail - diff = " << diff << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        std::cout << "test kernel pass - diff = " << diff << std::endl;
+    }
+    }
+
+    {
+    using TYPE = unsigned int;
+    auto gpu = std::make_shared<cle::GPU>();
+    gpu->WaitForKernelToFinish();
+
+        std::array<int,3> dims = {10, 1, 1};
+    std::vector<TYPE> data (dims[0]*dims[1]*dims[2]); 
+    std::vector<TYPE> valid (dims[0]*dims[1]*dims[2]); 
+    std::fill (data.begin(),data.end(), 10.0f);
+    std::fill (valid.begin(),valid.end(), 100.0f);
+
+    cle::Buffer buff_A = gpu->PushBuffer<TYPE>(data, dims);
+    cle::Buffer buff_B = gpu->CreateBuffer<TYPE>(dims);
+
+    cle::AddImageAndScalarKernel kernel(gpu);
+    kernel.SetInput(buff_A);
+    kernel.SetOutput(buff_B);
+    kernel.SetScalar(90.0f);
+    kernel.Execute();
+
+    std::vector<TYPE> res = gpu->Pull<TYPE>(buff_B);
+    
+
+    float diff = 0;
+    for (int i =0; i< res.size(); i++)
+    {
+        diff += valid[i] - res[i];
+    }
+    if (diff > 0)
+    {
+        std::cout << "test kernel fail - diff = " << diff << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        std::cout << "test kernel pass - diff = " << diff << std::endl;
+    }
+    }
+
+    {
+    using TYPE = char;
+    auto gpu = std::make_shared<cle::GPU>();
+    gpu->WaitForKernelToFinish();
+
+        std::array<int,3> dims = {10, 1, 1};
+    std::vector<TYPE> data (dims[0]*dims[1]*dims[2]); 
+    std::vector<TYPE> valid (dims[0]*dims[1]*dims[2]); 
+    std::fill (data.begin(),data.end(), 10.0f);
+    std::fill (valid.begin(),valid.end(), 100.0f);
+
+    cle::Buffer buff_A = gpu->PushBuffer<TYPE>(data, dims);
+    cle::Buffer buff_B = gpu->CreateBuffer<TYPE>(dims);
+
+    cle::AddImageAndScalarKernel kernel(gpu);
+    kernel.SetInput(buff_A);
+    kernel.SetOutput(buff_B);
+    kernel.SetScalar(90.0f);
+    kernel.Execute();
+
+    std::vector<TYPE> res = gpu->Pull<TYPE>(buff_B);
+    
+
+    float diff = 0;
+    for (int i =0; i< res.size(); i++)
+    {
+        diff += valid[i] - res[i];
+    }
+    if (diff > 0)
+    {
+        std::cout << "test kernel fail - diff = " << diff << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        std::cout << "test kernel pass - diff = " << diff << std::endl;
+    }
+    }
+
+    {
+    using TYPE = unsigned char;
+    auto gpu = std::make_shared<cle::GPU>();
+    gpu->WaitForKernelToFinish();
+
+        std::array<int,3> dims = {10, 1, 1};
+    std::vector<TYPE> data (dims[0]*dims[1]*dims[2]); 
+    std::vector<TYPE> valid (dims[0]*dims[1]*dims[2]); 
+    std::fill (data.begin(),data.end(), 10.0f);
+    std::fill (valid.begin(),valid.end(), 100.0f);
+
+    cle::Buffer buff_A = gpu->PushBuffer<TYPE>(data, dims);
+    cle::Buffer buff_B = gpu->CreateBuffer<TYPE>(dims);
+
+    cle::AddImageAndScalarKernel kernel(gpu);
+    kernel.SetInput(buff_A);
+    kernel.SetOutput(buff_B);
+    kernel.SetScalar(90.0f);
+    kernel.Execute();
+
+    std::vector<TYPE> res = gpu->Pull<TYPE>(buff_B);
+    
+
+    float diff = 0;
+    for (int i =0; i< res.size(); i++)
+    {
+        diff += valid[i] - res[i];
+    }
+    if (diff > 0)
+    {
+        std::cout << "test kernel fail - diff = " << diff << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        std::cout << "test kernel pass - diff = " << diff << std::endl;
+    }
+    }
+
+    {
+    using TYPE = short;
+    auto gpu = std::make_shared<cle::GPU>();
+    gpu->WaitForKernelToFinish();
+
+    std::array<int,3> dims = {10, 1, 1};
+    std::vector<TYPE> data (dims[0]*dims[1]*dims[2]); 
+    std::vector<TYPE> valid (dims[0]*dims[1]*dims[2]); 
+    std::fill (data.begin(),data.end(), 10.0f);
+    std::fill (valid.begin(),valid.end(), 100.0f);
+
+    cle::Buffer buff_A = gpu->PushBuffer<TYPE>(data, dims);
+    cle::Buffer buff_B = gpu->CreateBuffer<TYPE>(dims);
+
+    cle::AddImageAndScalarKernel kernel(gpu);
+    kernel.SetInput(buff_A);
+    kernel.SetOutput(buff_B);
+    kernel.SetScalar(90.0f);
+    kernel.Execute();
+
+    std::vector<TYPE> res = gpu->Pull<TYPE>(buff_B);
+    
+
+    float diff = 0;
+    for (int i =0; i< res.size(); i++)
+    {
+        diff += valid[i] - res[i];
+    }
+    if (diff > 0)
+    {
+        std::cout << "test kernel fail - diff = " << diff << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        std::cout << "test kernel pass - diff = " << diff << std::endl;
+    }
+    }
+
+    {
+    using TYPE = unsigned short;
+    auto gpu = std::make_shared<cle::GPU>();
+    gpu->WaitForKernelToFinish();
+
+    std::array<int,3> dims = {10, 1, 1};
+    std::vector<TYPE> data (dims[0]*dims[1]*dims[2]); 
+    std::vector<TYPE> valid (dims[0]*dims[1]*dims[2]); 
+    std::fill (data.begin(),data.end(), 10.0f);
+    std::fill (valid.begin(),valid.end(), 100.0f);
+
+    cle::Buffer buff_A = gpu->PushBuffer<TYPE>(data, dims);
+    cle::Buffer buff_B = gpu->CreateBuffer<TYPE>(dims);
+
+    cle::AddImageAndScalarKernel kernel(gpu);
+    kernel.SetInput(buff_A);
+    kernel.SetOutput(buff_B);
+    kernel.SetScalar(90.0f);
+    kernel.Execute();
+
+    std::vector<TYPE> res = gpu->Pull<TYPE>(buff_B);
+    
+
+    float diff = 0;
+    for (int i =0; i< res.size(); i++)
+    {
+        diff += valid[i] - res[i];
+    }
+    if (diff > 0)
+    {
+        std::cout << "test kernel fail - diff = " << diff << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        std::cout << "test kernel pass - diff = " << diff << std::endl;
+    }
+    }
 
     return EXIT_SUCCESS;
 }
