1b1b877e22dc3884ee8381055c3b2fe956c5227e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-03-01 18:39:42 +0100

    FIX: more warning

--- clic/src/tier1/cleGaussianBlurKernel.cpp
+++ clic/src/tier1/cleGaussianBlurKernel.cpp
@@ -19,7 +19,7 @@ std::array<int,3> GaussianBlurKernel::Sigma2KernelSize(std::array<float,3> t_sig
     std::array<int,3> ksize = {0,0,0};
     auto it_size (ksize.begin()), size_end (ksize.end());
     auto it_sigma (t_sigma.begin()), sigma_end (t_sigma.end());
-    for( ; it_sigma != sigma_end, it_size != size_end; ++it_sigma, ++it_size)
+    for( ; (it_sigma != sigma_end) && (it_size != size_end); ++it_sigma, ++it_size)
     {
         *it_size = static_cast<int>(*it_sigma * 8.0 + 0.5);
         if (*it_size % 2 == 0)
