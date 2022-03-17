681ff0034eacdacf30279db574d21a9d98c7b587
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-03-17 14:51:33 +0100

    ADD: test python wrapper

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from skbuild import setup
 setup(
     name="pyclic",
     version="0.5.2",
+    description="GPU accelerated image processing library",
+    author="Stephane Rigaud",
+    license="BDS",
     packages=['pyclic'],
     package_dir={'': 'python'},
-    cmake_install_dir='python/pyclic')
+    cmake_install_dir='python/pyclic',
+    python_requires='>=3.7',
+    )
 
 # When building extension modules `cmake_install_dir` should always be set to the
 # location of the package you are building extension modules for.
