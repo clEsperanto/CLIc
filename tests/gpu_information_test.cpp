4628f11c92dee579e3f9f0e731db5c918c0d5467
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-04-08 15:25:46 +0200

    ENH: list available devices method

--- tests/gpu_information_test.cpp
+++ tests/gpu_information_test.cpp
@@ -5,6 +5,9 @@ int main(int argc, char **argv)
 {
 
     cle::Clesperanto cle;
+    std::cout << "Devices available:\n";
+    std::cout << cle.Ressources()->ListDevices() << std::endl << std::endl;
+    std::cout << "Device info:\n";
     std::cout << cle.Ressources()->Info() << std::endl;
 
     return EXIT_SUCCESS;
