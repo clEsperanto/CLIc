0aac39607b9d425d3b4160fd9cd224529efb534f
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-08-02 14:13:14 +0200

    STYLE: remove cout

--- tests/extend_labeling_via_voronoi_test.cpp
+++ tests/extend_labeling_via_voronoi_test.cpp
@@ -47,21 +47,6 @@ run_test (const std::array<size_t, 3> &shape, const cl_mem_object_type &mem_type
     cle.ExtendLabelingViaVoronoi (gpu_input, gpu_output);
     auto output = cle.Pull<type> (gpu_output);
 
-    std::copy (std::begin (input),
-               std::end (input),
-               std::ostream_iterator<type> (std::cout, ", "));
-    std::cout << std::endl;
-
-    std::copy (std::begin (valid),
-               std::end (valid),
-               std::ostream_iterator<type> (std::cout, ", "));
-    std::cout << std::endl;
-
-    std::copy (std::begin (output),
-               std::end (output),
-               std::ostream_iterator<type> (std::cout, ", "));
-    std::cout << std::endl;
-
     return std::equal (output.begin (), output.end (), valid.begin ());
 }
 
