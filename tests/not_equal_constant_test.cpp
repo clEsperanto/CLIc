541d62babf342656d42357bf99b6a77227c4f42c
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-11-16 11:25:22 +0100

    remove image test

--- tests/not_equal_constant_test.cpp
+++ tests/not_equal_constant_test.cpp
@@ -9,6 +9,11 @@ template <class type>
 auto
 run_test(const std::array<size_t, 3> & shape, const cle::MemoryType & mem_type) -> bool
 {
+
+  std::cout << shape[0] << "," << shape[1] << "," << shape[2] << " - " << typeid(type).name() << " - " << mem_type
+            << std::endl;
+
+
   std::vector<type>                         input(shape[0] * shape[1] * shape[2]);
   std::vector<type>                         valid(shape[0] * shape[1] * shape[2]);
   static std::uniform_int_distribution<int> distribution(1, 10);
@@ -16,12 +21,33 @@ run_test(const std::array<size_t, 3> & shape, const cle::MemoryType & mem_type) 
   std::generate(input.begin(), input.end(), []() { return static_cast<type>(distribution(generator)); });
   std::transform(input.begin(), input.end(), valid.begin(), [](const type & x) { return x != 5; });
 
+  std::cout << "input: ";
+  for (auto && i : input)
+  {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+  std::cout << "valid: ";
+  for (auto && i : valid)
+  {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+
   cle::Clesperanto cle;
   cle.GetDevice()->WaitForKernelToFinish();
   auto gpu_input = cle.Push<type>(input, shape, mem_type);
   auto gpu_output = cle.Create<type>(shape, mem_type);
   cle.NotEqualConstant(gpu_input, gpu_output, 5);
   auto output = cle.Pull<type>(gpu_output);
+
+  std::cout << "output: ";
+  for (auto && i : output)
+  {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+  std::cout << std::endl;
 
   return std::equal(output.begin(), output.end(), valid.begin());
 }
@@ -136,108 +162,108 @@ main(int argc, char ** argv) -> int
 
   // if (!run_test<float>({ 10, 1, 1 }, cle::IMAGE))
   // {
-  // return EXIT_FAILURE;
+  //   return EXIT_FAILURE;
   // }
 
-  if (!run_test<signed int>({ 10, 1, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed int>({ 10, 1, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned int>({ 10, 1, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned int>({ 10, 1, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<signed short>({ 10, 1, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed short>({ 10, 1, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned short>({ 10, 1, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned short>({ 10, 1, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<signed char>({ 10, 1, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed char>({ 10, 1, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned char>({ 10, 1, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned char>({ 10, 1, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
   // if (!run_test<float>({ 10, 7, 1 }, cle::IMAGE))
   // {
-  // return EXIT_FAILURE;
+  //   return EXIT_FAILURE;
   // }
 
-  if (!run_test<signed int>({ 10, 7, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed int>({ 10, 7, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned int>({ 10, 7, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned int>({ 10, 7, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<signed short>({ 10, 7, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed short>({ 10, 7, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned short>({ 10, 7, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned short>({ 10, 7, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<signed char>({ 10, 7, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed char>({ 10, 7, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned char>({ 10, 7, 1 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned char>({ 10, 7, 1 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
   // if (!run_test<float>({ 10, 7, 5 }, cle::IMAGE))
   // {
-  // return EXIT_FAILURE;
+  //   return EXIT_FAILURE;
   // }
 
-  if (!run_test<signed int>({ 10, 7, 5 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed int>({ 10, 7, 5 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned int>({ 10, 7, 5 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned int>({ 10, 7, 5 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<signed short>({ 10, 7, 5 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed short>({ 10, 7, 5 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned short>({ 10, 7, 5 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned short>({ 10, 7, 5 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<signed char>({ 10, 7, 5 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<signed char>({ 10, 7, 5 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
-  if (!run_test<unsigned char>({ 10, 7, 5 }, cle::IMAGE))
-  {
-    return EXIT_FAILURE;
-  }
+  // if (!run_test<unsigned char>({ 10, 7, 5 }, cle::IMAGE))
+  // {
+  //   return EXIT_FAILURE;
+  // }
 
   return EXIT_SUCCESS;
 }
