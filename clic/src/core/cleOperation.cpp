684e5d0dd73caa1cb2aed1fe204a4d9614d65155
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-03-31 15:57:39 +0200

    TEST: remove preamble access

--- clic/src/core/cleOperation.cpp
+++ clic/src/core/cleOperation.cpp
@@ -178,7 +178,7 @@ Operation::GetKernel() const -> const cl::Kernel &
 auto
 Operation::MakePreamble() -> std::string
 {
-  return std::string(oclKernel::preamble);
+  return ""; // oclKernel::preamble;
 }
 
 auto
