62fad93bd19eb1233cc3e4d5a28d142b96b65fd5
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-04-20 17:07:15 +0200

    ENH: simplify custom call

--- clic/include/tier1/cleCustomKernel.hpp
+++ clic/include/tier1/cleCustomKernel.hpp
@@ -15,22 +15,39 @@ public:
                         const size_t &           nb_parameters);
 
   auto
-  AddImages(const std::map<std::string, Image> & images) -> void;
+  AddImage(const std::string & tag, const cle::Image & object) -> void;
 
   auto
-  AddScalars(const std::map<std::string, float> & scalars) -> void;
+  AddScalar(const std::string & tag, const float & value) -> void;
+
+  auto
+  AddScalar(const std::string & tag, const int & value) -> void;
 };
 
 inline auto
 CustomKernel_Call(const std::shared_ptr<cle::Processor> & device,
                   const std::string &                     filename,
                   const std::string &                     name,
-                  const std::map<std::string, Image> &    images,
-                  const std::map<std::string, float> &    scalars) -> void
+                  const cle::ParametersMap &              parameters,
+                  const std::array<size_t, 3> &           global_range = { 1, 1, 1 }) -> void
 {
-  CustomKernel kernel(device, filename, name, images.size() + scalars.size());
-  kernel.AddImages(images);
-  kernel.AddScalars(scalars);
+  CustomKernel kernel(device, filename, name, parameters.size());
+  for (auto ite = parameters.begin(); ite != parameters.end(); ite++)
+  {
+    if (std::holds_alternative<cle::Image>(ite->second))
+    {
+      kernel.AddImage(ite->first, std::get<cle::Image>(ite->second));
+    }
+    else if (std::holds_alternative<float>(ite->second))
+    {
+      kernel.AddScalar(ite->first, std::get<float>(ite->second));
+    }
+    else if (std::holds_alternative<int>(ite->second))
+    {
+      kernel.AddScalar(ite->first, std::get<int>(ite->second));
+    }
+  }
+  kernel.SetRange(global_range);
   kernel.Execute();
 }
 
