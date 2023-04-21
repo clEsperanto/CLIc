aa8e35ffa176858ba67dcea5e8443ef5149d8af0
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-04-21 15:47:39 +0200

    STYLE: remove cout debug

--- clic/include/tier1/cleCustomKernel.hpp
+++ clic/include/tier1/cleCustomKernel.hpp
@@ -38,23 +38,17 @@ CustomKernel_Call(const std::shared_ptr<cle::Processor> &                       
   {
     if (std::holds_alternative<cle::Image>(ite->second))
     {
-      std::cout << ite->first << " : " << std::get<cle::Image>(ite->second) << std::endl;
       kernel.AddImage(ite->first, std::get<cle::Image>(ite->second));
     }
     else if (std::holds_alternative<float>(ite->second))
     {
-      std::cout << ite->first << " : " << std::get<float>(ite->second) << std::endl;
       kernel.AddScalar(ite->first, std::get<float>(ite->second));
     }
     else if (std::holds_alternative<int>(ite->second))
     {
-      std::cout << ite->first << " : " << std::get<int>(ite->second) << std::endl;
       kernel.AddScalar(ite->first, std::get<int>(ite->second));
     }
   }
-  std::cout << "kernel = " << file_name << " -> " << kernel_name << std::endl;
-  std::cout << "device = " << device->GetDeviceName() << std::endl;
-  std::cout << "global ranges: " << dx << "," << dy << "," << dz << std::endl;
   kernel.SetRange({ dx, dy, dz });
   kernel.Execute();
 }
