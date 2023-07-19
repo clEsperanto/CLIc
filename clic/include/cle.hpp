b893b6c85660206bdfc8b8787759e6be4f251e0d
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-19 12:38:07 +0200

    enables tiers up to 5

--- clic/include/cle.hpp
+++ clic/include/cle.hpp
@@ -8,9 +8,9 @@
 
 #include "tier1.hpp"
 #include "tier2.hpp"
-// #include "tier3.hpp"
-// #include "tier4.hpp"
-// #include "tier5.hpp"
+#include "tier3.hpp"
+#include "tier4.hpp"
+#include "tier5.hpp"
 // #include "tier6.hpp"
 // #include "tier7.hpp"
 // #include "tier8.hpp"
