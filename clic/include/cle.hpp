53dd9938bbfe6ab38ee6fe0e441ed55d54d5a950
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-25 11:46:05 +0200

    add: tier6 and 7 to build

--- clic/include/cle.hpp
+++ clic/include/cle.hpp
@@ -11,8 +11,8 @@
 #include "tier3.hpp"
 #include "tier4.hpp"
 #include "tier5.hpp"
-// #include "tier6.hpp"
-// #include "tier7.hpp"
+#include "tier6.hpp"
+#include "tier7.hpp"
 // #include "tier8.hpp"
 
 #endif // __INCLUDE_CLE_HPP
