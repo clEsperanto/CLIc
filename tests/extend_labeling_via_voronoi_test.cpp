2080d3fecbf755bbcb41bf9483bda49a6d6aa9fd
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-25 13:58:02 +0200

    update tests

--- tests/extend_labeling_via_voronoi_test.cpp
+++ tests/extend_labeling_via_voronoi_test.cpp
@@ -33,28 +33,28 @@ main(int argc, char ** argv) -> int
 {
   cle::BackendManager::getInstance().setBackend("opencl");
   std::cout << cle::BackendManager::getInstance().getBackend() << " backend selected" << std::endl;
-  assert(run_test<float>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  // assert(run_test<int64_t>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  assert(run_test<int32_t>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  assert(run_test<int16_t>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  assert(run_test<int8_t>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  // assert(run_test<float>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
-  // assert(run_test<int64_t>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
-  // assert(run_test<int32_t>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
-  // assert(run_test<int16_t>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
-  // assert(run_test<int8_t>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
+  assert(run_test<float>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  // assert(run_test<int64_t>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  assert(run_test<int32_t>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  assert(run_test<int16_t>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  assert(run_test<int8_t>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  // assert(run_test<float>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
+  // assert(run_test<int64_t>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
+  // assert(run_test<int32_t>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
+  // assert(run_test<int16_t>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
+  // assert(run_test<int8_t>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
 
   cle::BackendManager::getInstance().setBackend("cuda");
   std::cout << cle::BackendManager::getInstance().getBackend() << " backend selected" << std::endl;
-  assert(run_test<float>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  // assert(run_test<int64_t>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  assert(run_test<int32_t>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  assert(run_test<int16_t>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  assert(run_test<int8_t>({ 5, 5, 2 }, cle::mType::BUFFER) == 0);
-  // assert(run_test<int64_t>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
-  // assert(run_test<int32_t>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
-  // assert(run_test<int16_t>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
-  // assert(run_test<int8_t>({ 5, 5, 2 }, cle::mType::IMAGE) == 0);
+  assert(run_test<float>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  // assert(run_test<int64_t>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  assert(run_test<int32_t>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  assert(run_test<int16_t>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  assert(run_test<int8_t>({ 6, 3, 2 }, cle::mType::BUFFER) == 0);
+  // assert(run_test<int64_t>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
+  // assert(run_test<int32_t>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
+  // assert(run_test<int16_t>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
+  // assert(run_test<int8_t>({ 6, 3, 2 }, cle::mType::IMAGE) == 0);
 
   return EXIT_SUCCESS;
 }
