88ce412d86e177e752af9e00b142d3a0c611ff34
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-25 13:58:39 +0200

    fix: force float for gaussian blur

--- clic/src/tier1.cpp
+++ clic/src/tier1.cpp
@@ -606,11 +606,17 @@ gaussian_blur_func(const Device::Pointer & device,
                    float                   sigma_y,
                    float                   sigma_z) -> Array::Pointer
 {
-  tier0::create_like(src, dst);
+  tier0::create_like(src, dst, dType::FLOAT);
+  Array::Pointer temp = src;
+  if (temp->dtype() != dType::FLOAT)
+  {
+    temp = Array::create(dst);
+    tier1::copy_func(device, src, temp);
+  }
   const KernelInfo kernel = { "gaussian_blur_separable", kernel::gaussian_blur_separable };
   tier0::execute_separable_func(device,
                                 kernel,
-                                src,
+                                temp,
                                 dst,
                                 { sigma_x, sigma_y, sigma_z },
                                 { sigma2kernelsize(sigma_x), sigma2kernelsize(sigma_y), sigma2kernelsize(sigma_z) });
