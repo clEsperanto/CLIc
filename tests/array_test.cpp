466c2949433d8b1e89573545cd480443e7a551b2
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-09-27 18:59:27 +0200

    update array test

--- tests/array_test.cpp
+++ tests/array_test.cpp
@@ -18,8 +18,8 @@ run_test(const std::array<size_t, 3> & shape, const cle::mType & mem_type) -> bo
     gpu_input->write(input.data());
     auto gpu_copy = cle::Array::create(gpu_input);
     gpu_input->copy(gpu_copy);
-    std::vector<type> input_test(gpu_copy->size());
-    gpu_copy->read(input_test.data());
+    std::vector<type> input_test(gpu_input->size());
+    gpu_input->read(input_test.data());
     std::equal(input_test.begin(), input_test.end(), input.begin()) ? std::cout << "all good\n"
                                                                     : std::cout << "not good\n";
   }
@@ -49,7 +49,6 @@ run_test(const std::array<size_t, 3> & shape, const cle::mType & mem_type) -> bo
     std::equal(input_test.begin(), input_test.end(), input.begin()) ? std::cout << "all good\n"
                                                                     : std::cout << "not good\n";
   }
-
   {
     type input = -7;
     std::cout << "test one: create, write, copy, read: ... ";
@@ -60,6 +59,52 @@ run_test(const std::array<size_t, 3> & shape, const cle::mType & mem_type) -> bo
     type input_test;
     gpu_copy->read(&input_test);
     (input_test == input) ? std::cout << "all good\n" : std::cout << "not good\n";
+  }
+
+  {
+    std::vector<type> input(10 * 10 * 1);
+    std::fill(input.begin(), input.end(), static_cast<type>(-7));
+    std::cout << "test region: create, write, copy, read: ... ";
+    auto gpu_input = cle::Array::create(10, 10, 1, cle::toType<type>(), mem_type, device);
+    gpu_input->write(input.data());
+
+    std::vector<type> subinput(5 * 5 * 1);
+    std::fill(subinput.begin(), subinput.end(), static_cast<type>(1));
+
+    gpu_input->write(subinput.data(), { 5, 5, 1 }, { 3, 1, 0 });
+    std::vector<type> test(gpu_input->size());
+    gpu_input->read(test.data());
+
+    // for (int i = 0; i < test.size(); i++)
+    // {
+    //   if (i % 10 == 0)
+    //     std::cout << std::endl;
+    //   std::cout << test[i] << " ";
+    // }
+    // std::cout << std::endl;
+
+    std::array<size_t, 3> region = { 6, 3, 1 };
+    std::vector<type>     subtest(region[0] * region[1] * region[2]);
+    gpu_input->read(subtest.data(), region, { 1, 1, 0 });
+
+    // for (int i = 0; i < subtest.size(); i++)
+    // {
+    //   if (i % region[0] == 0)
+    //     std::cout << std::endl;
+    //   std::cout << subtest[i] << " ";
+    // }
+    // std::cout << std::endl;
+
+    gpu_input->fill(0);
+    gpu_input->read(input.data());
+
+    // for (int i = 0; i < input.size(); i++)
+    // {
+    //   if (i % 10 == 0)
+    //     std::cout << std::endl;
+    //   std::cout << input[i] << " ";
+    // }
+    // std::cout << std::endl;
   }
 
 
