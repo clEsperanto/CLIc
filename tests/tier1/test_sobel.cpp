4cdc5902c337c9fbf02b9259b1bb40e333389f8e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-10-20 10:48:05 +0200

    fix failing test sobel

--- tests/tier1/test_sobel.cpp
+++ tests/tier1/test_sobel.cpp
@@ -6,21 +6,13 @@
 class TestSobel : public ::testing::TestWithParam<std::string>
 {
 protected:
-  std::array<float, 5 * 5 * 3> output;
-  std::array<float, 5 * 5 * 3> input = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-  std::array<float, 5 * 5 * 3> valid = {
-    0., 0.,         0.,         0.,         0., 0., 0.4375,     0.71443451, 0.4375,     0.,
-    0., 0.71443451, 1.01036297, 0.71443451, 0., 0., 0.4375,     0.71443451, 0.4375,     0.,
-    0., 0.,         0.,         0.,         0., 0., 0.,         0.,         0.,         0.,
-    0., 0.71443451, 1.01036297, 0.71443451, 0., 0., 1.01036297, 0.,         1.01036297, 0.,
-    0., 0.71443451, 1.01036297, 0.71443451, 0., 0., 0.,         0.,         0.,         0.,
-    0., 0.,         0.,         0.,         0., 0., 0.4375,     0.71443451, 0.4375,     0.,
-    0., 0.71443451, 1.01036297, 0.71443451, 0., 0., 0.4375,     0.71443451, 0.4375,     0.,
-    0., 0.,         0.,         0.,         0.
-  };
+  std::array<float, 5 * 5 * 1> output;
+  std::array<float, 5 * 5 * 1> input = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+  std::array<float, 5 * 5 * 1> valid = { 0,       0, 0,       0,       0,       0,       1.41421, 2,       1.41421,
+                                         0,       0, 3.16228, 2,       3.16228, 0,       0,       3.16228, 2,
+                                         3.16228, 0, 0,       1.41421, 2,       1.41421, 0 };
 };
+
 
 TEST_P(TestSobel, execute)
 {
@@ -28,7 +20,7 @@ TEST_P(TestSobel, execute)
   cle::BackendManager::getInstance().setBackend(param);
   auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "all");
 
-  auto gpu_input = cle::Array::create(5, 5, 3, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  auto gpu_input = cle::Array::create(5, 5, 1, cle::dType::FLOAT, cle::mType::BUFFER, device);
   gpu_input->write(input.data());
 
   auto gpu_output = cle::tier1::sobel_func(device, gpu_input, nullptr);
