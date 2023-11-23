785a2338181167d64ef864c6964b504384914a87
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-11-23 17:53:04 +0100

    add test and function

--- clic/src/tier1.cpp
+++ clic/src/tier1.cpp
@@ -1460,12 +1460,17 @@ range_func(const Device::Pointer & device,
   correct_range(&start_x, &stop_x, &step_x, static_cast<int>(src->width()));
   correct_range(&start_y, &stop_y, &step_y, static_cast<int>(src->height()));
   correct_range(&start_z, &stop_z, &step_z, static_cast<int>(src->depth()));
-  tier0::create_dst(src, dst, abs(start_x - stop_x), abs(start_y - stop_y), abs(start_z - stop_z), src->dtype());
+  tier0::create_dst(src,
+                    dst,
+                    abs(stop_x - start_x) / std::max(step_x, 1),
+                    abs(stop_y - start_y) / std::max(step_y, 1),
+                    abs(stop_z - start_z) / std::max(step_z, 1),
+                    src->dtype());
   const KernelInfo    kernel = { "range", kernel::range };
   const ParameterList params = { { "src", src },         { "dst", dst },         { "start_x", start_x },
-                                 { "start_y", start_y }, { "start_z", start_z }, { "step_x", step_x },
-                                 { "step_y", step_y },   { "step_z", step_z } };
-  const RangeArray    range = { 1, 1, 1 };
+                                 { "step_x", step_x },   { "start_y", start_y }, { "step_y", step_y },
+                                 { "start_z", start_z }, { "step_z", step_z } };
+  const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
   execute(device, kernel, params, range);
   return dst;
 }
