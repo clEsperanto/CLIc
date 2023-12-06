8bec0dd18e002572f6e4c976a686b543309f0b95
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-12-06 15:30:21 +0100

    rm pts list check

--- clic/src/tier1.cpp
+++ clic/src/tier1.cpp
@@ -1501,10 +1501,10 @@ read_values_from_coordinates_func(const Device::Pointer & device,
                                   const Array::Pointer &  list,
                                   Array::Pointer          dst) -> Array::Pointer
 {
-  if (list->dim() == src->dim())
-  {
-    throw std::runtime_error("The list input is expected to be 3D, where rows are coordinates (x,y,z)");
-  }
+  // if (list->dim() == src->dim())
+  // {
+  //   throw std::runtime_error("The list input is expected to be 3D, where rows are coordinates (x,y,z)");
+  // }
   tier0::create_vector(src, dst, src->width());
   const KernelInfo    kernel = { "read_values_from_coordinates", kernel::read_values_from_coordinates };
   const ParameterList params = { { "src0", src }, { "src1", list }, { "dst", dst } };
