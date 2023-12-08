29ccbc6163ab33f7b5bdad09a34855af2d476802
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-12-08 17:18:55 +0100

    default dst to float

--- clic/src/tier1.cpp
+++ clic/src/tier1.cpp
@@ -1589,7 +1589,7 @@ multiply_matrix_func(const Device::Pointer & device,
 auto
 reciprocal_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst) -> Array::Pointer
 {
-  tier0::create_like(src, dst);
+  tier0::create_like(src, dst, dType::FLOAT);
   const KernelInfo    kernel = { "reciprocal", kernel::reciprocal };
   const ParameterList params = { { "src", src }, { "dst", dst } };
   const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
