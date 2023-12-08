8c25d2be8bfcedf6aa333c332eb8f8a05ab2d78d
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-12-08 16:02:09 +0100

    default squared_difference_func dst set to float

--- clic/src/tier2.cpp
+++ clic/src/tier2.cpp
@@ -424,7 +424,7 @@ squared_difference_func(const Device::Pointer & device,
                         const Array::Pointer &  src1,
                         Array::Pointer          dst) -> Array::Pointer
 {
-  tier0::create_like(src0, dst);
+  tier0::create_like(src0, dst, dType::FLOAT);
   auto tmp = tier1::add_images_weighted_func(device, src0, src1, nullptr, 1, -1);
   return tier1::power_func(device, tmp, dst, 2);
 }
