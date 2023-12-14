99f098342e18953da6633c819870be4c3e85f085
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-12-14 14:20:06 +0100

    add spot to point lists

--- clic/src/tier4.cpp
+++ clic/src/tier4.cpp
@@ -9,6 +9,24 @@
 namespace cle::tier4
 {
 
+// auto proximal_other_labels_count_map_func
+
+// auto erode_connected_labels_func
+// auto exclude_labels_with_average_values_out_of_range_func
+// auto exclude_labels_with_average_values_within_range_func
+// auto exclude_labels_with_map_values_equal_to_constant_func
+// auto exclude_labels_with_map_values_not_equal_to_constant_func
+// auto exclude_labels_with_map_values_out_of_range_func
+// auto exclude_labels_with_map_values_within_range_func
+// auto exclude_large_labels_func
+// auto exclude_small_labels_func
+// auto extend_labeling_via_voronoi_func
+
+// auto extended_depth_of_focus_variance_projection_func
+// auto generate_n_most_touching_neighbors_matrix_func
+// auto generate_touch_portion_matrix_func
+// auto generate_touch_portion_within_range_neighbors_matrix_func
+
 auto
 mean_squared_error_func(const Device::Pointer & device, const Array::Pointer & src0, const Array::Pointer & src1)
   -> float
@@ -16,6 +34,32 @@ mean_squared_error_func(const Device::Pointer & device, const Array::Pointer & s
   auto temp = tier2::squared_difference_func(device, src0, src1, nullptr);
   return tier3::mean_of_all_pixels_func(device, temp);
 }
+
+// auto label_nonzero_pixel_count_map_func
+// auto label_nonzero_pixel_count_ratio_map_func
+// auto label_overlap_count_map_func
+// auto local_maximum_touching_neighbor_count_map_func
+// auto local_mean_touching_neighbor_count_map_func
+// auto local_median_touching_neighbor_count_map_func
+// auto local_minimum_touching_neighbor_count_map_func
+// auto local_standard_deviation_touching_neighbor_count_map_func
+// auto merge_touching_labels_func
+// auto proximal_neighbor_count_func
+// auto proximal_neighbor_count_map_func
+// auto sorensen_dice_coefficient_func
+
+auto
+spots_to_pointlist_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst)
+  -> Array::Pointer
+{
+  auto labeled_spots = tier2::label_spots_func(device, src, nullptr);
+  return tier3::labelled_spots_to_pointlist_func(device, labeled_spots, dst);
+}
+
+// auto subtract_labels_func
+// auto touch_portion_within_range_neighbor_count_func
+// auto touch_portion_within_range_neighbor_count_map_func
+// auto touching_neighbor_count_map_func
 
 auto
 relabel_sequential_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst, int blocksize)
