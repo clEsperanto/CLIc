c344e3dfc0af5e76c18a91c0644ce3ec1a4efcd2
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-02-26 16:40:28 +0100

    adding smooth_labels + tier8
    Co-Authored-By: Stephane Rigaud <811146+StRigaud@users.noreply.github.com>

---
+++ tests/tier8/test_smooth_labels.cpp
@@ -0,0 +1,66 @@
+#include "cle.hpp"
+
+#include <array>
+#include <gtest/gtest.h>
+
+class TestSmoothLabels : public ::testing::TestWithParam<std::string>
+{
+protected:
+  const std::array<uint32_t, 8 * 9 * 1> input = {
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 2, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 1, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2
+  };
+  const std::array<uint32_t, 8 * 9 * 1> valid = {
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2,
+    1, 1, 1, 1, 2, 2, 2, 2
+  };
+  std::array<uint32_t, 8 * 9 * 1> output;
+};
+
+TEST_P(TestSmoothLabels, execute)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("RTX", "all");
+  device->setWaitToFinish(true);
+
+  auto gpu_input = cle::Array::create(8, 9, 1, 2, cle::dType::UINT32, cle::mType::BUFFER, device);
+  gpu_input->write(input.data());
+
+  auto gpu_output = cle::tier8::smooth_labels_func(device, gpu_input, nullptr, 3);
+
+  gpu_output->read(output.data());
+  for (int i = 0; i < output.size(); i++)
+  {
+    EXPECT_EQ(output[i], valid[i]);
+  }
+}
+
+std::vector<std::string>
+getParameters()
+{
+  std::vector<std::string> parameters;
+#if USE_OPENCL
+  parameters.push_back("opencl");
+#endif
+#if USE_CUDA
+  parameters.push_back("cuda");
+#endif
+  return parameters;
+}
+
+INSTANTIATE_TEST_SUITE_P(InstantiationName, TestSmoothLabels, ::testing::ValuesIn(getParameters()));
