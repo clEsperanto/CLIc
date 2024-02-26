b9a177f2539c478e0db3bf5329221ebbf5536492
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-02-26 17:20:01 +0100

    fix format

--- clic/src/tier8.cpp
+++ clic/src/tier8.cpp
@@ -12,23 +12,18 @@
 namespace cle::tier8
 {
 
-
 auto
 smooth_labels_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst, int radius)
   -> Array::Pointer
 {
   tier0::create_like(src, dst, dType::UINT32);
-
-  if (radius < 1) {
+  if (radius < 1)
+  {
     return tier1::copy_func(device, src, dst);
   }
-  
   auto binary = tier1::greater_constant_func(device, src, nullptr, 0);
-
   auto opened = tier7::opening_labels_func(device, src, nullptr, radius);
-
   auto extended = tier2::extend_labeling_via_voronoi_func(device, opened, nullptr);
-
   return tier1::multiply_images_func(device, binary, extended, dst);
 }
 
