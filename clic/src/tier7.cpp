9df9ec7ac95c554d801b01f72f9b9efe83ca0d0f
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-02-28 14:18:07 +0100

    add erode otsu labeling function

--- clic/src/tier7.cpp
+++ clic/src/tier7.cpp
@@ -13,17 +13,28 @@
 namespace cle::tier7
 {
 
-// auto
-// affine_transform_func(const Device::Pointer & device,
-//                       const Array::Pointer &  src,
-//                       Array::Pointer          dst,
-//                       std::array<float, 16>   transform,
-//                       bool                    interpolate,
-//                       bool                    resize) -> Array::Pointer
-// {
-//   auto transform_matrix = AffineTransform(transform);
-//   return apply_affine_transform(src, dst, transform_matrix, interpolate, resize);
-// }
+auto
+erode_otsu_labeling_func(const Device::Pointer & device,
+                         const Array::Pointer &  src,
+                         Array::Pointer          dst,
+                         int                     number_of_erosion,
+                         float                   outline_sigma) -> Array::Pointer
+{
+  tier0::create_like(src, dst, dType::UINT32);
+  auto           blurred = tier1::gaussian_blur_func(device, src, nullptr, outline_sigma, outline_sigma, outline_sigma);
+  auto           binary = tier4::threshold_otsu_func(device, blurred, nullptr);
+  Array::Pointer eroded1 = nullptr;
+  Array::Pointer eroded2 = nullptr;
+  tier0::create_like(binary, eroded1);
+  tier0::create_like(binary, eroded2);
+  binary->copy(eroded1);
+  for (int i = 0; i < number_of_erosion; i++)
+  {
+    tier1::erode_box_func(device, eroded1, eroded2);
+    std::swap(eroded1, eroded2);
+  }
+  return tier6::masked_voronoi_labeling_func(device, eroded1, binary, dst);
+}
 
 
 auto
