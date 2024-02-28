382f142f4888763b016ec637624ae68c420402e3
Stephane Rigaud <Stephane.rigaud@pasteur.fr>
2024-02-28 11:11:04 +0100

    Update clic/src/openclbackend.cpp
    Co-authored-by: Robert Haase <haesleinhuepf@users.noreply.github.com>
    Signed-off-by: Stephane Rigaud <Stephane.rigaud@pasteur.fr>

--- clic/src/openclbackend.cpp
+++ clic/src/openclbackend.cpp
@@ -997,7 +997,7 @@ static auto
 buildProgram(const Device::Pointer & device, const cl_program & program) -> void
 {
   auto   opencl_device = std::dynamic_pointer_cast<const OpenCLDevice>(device);
-  cl_int buildStatus = clBuildProgram(program, 1, &opencl_device->getCLDevice(), "-w -cl-mad-enable", nullptr, nullptr);
+  cl_int buildStatus = clBuildProgram(program, 1, &opencl_device->getCLDevice(), "-w", nullptr, nullptr);
   if (buildStatus != CL_SUCCESS)
   {
     size_t      len;
