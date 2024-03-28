701fabe06e989c71d3133f419b67feb4f0af638d
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-03-28 18:30:10 +0100

    replace UINT32 with LABEL

--- clic/src/tier8.cpp
+++ clic/src/tier8.cpp
@@ -32,7 +32,7 @@ auto
 smooth_connected_labels_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst, int radius)
   -> Array::Pointer
 {
-  tier0::create_like(src, dst, dType::UINT32);
+  tier0::create_like(src, dst, dType::LABEL);
   if (radius < 1)
   {
     return tier1::copy_func(device, src, dst);
