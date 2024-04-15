0f1fffdd128c3b9fba027b46ed1bd99abde602ce
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-04-15 16:59:50 +0200

    format

--- clic/src/tier2/degrees_to_radians.cpp
+++ clic/src/tier2/degrees_to_radians.cpp
@@ -1,10 +1,10 @@
 #include "tier0.hpp"
 #include "tier1.hpp"
-#include "tier2.hpp" 
+#include "tier2.hpp"
 
 #include "utils.hpp"
 
-namespace cle::tier2 
+namespace cle::tier2
 {
 
 auto
@@ -13,7 +13,6 @@ degrees_to_radians_func(const Device::Pointer & device, const Array::Pointer & s
 {
   tier0::create_like(src, dst, dType::FLOAT);
   return tier1::multiply_image_and_scalar_func(device, src, dst, static_cast<float>(M_PI) / 180.0);
-}  
-
 }
 
+} // namespace cle::tier2
