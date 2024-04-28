3c0de8948109ae2e72d808936db9380c7ab59fa1
thawn <webmaster@korten.at>
2024-04-28 16:13:50 +0200

    fix the return type of multiply_image_and_position
    for images of type int8, multiply_image_and_position was limited to positions < 256 (and for int16 to positions < 2^16), because the return type was the same as the image type. However int8 images can have positions larger than that because position indices are of type int32. This commit ensures that the return type is at least int32 so that multiply_image_and_position can handle positions larger than the maximum integer for int8 and int16 images.
    
    closes issue tier1::multiply_image_and_position_func() implicitly assumes that the position has the same type as the input. #287

--- clic/src/tier1/multiply_image_and_position.cpp
+++ clic/src/tier1/multiply_image_and_position.cpp
@@ -14,7 +14,21 @@ multiply_image_and_position_func(const Device::Pointer & device,
                                  Array::Pointer          dst,
                                  int                     dimension) -> Array::Pointer
 {
-  tier0::create_like(src, dst);
+  auto type = src->dtype();
+  switch (src->dtype())
+  {
+    case dType::INT8:
+    case dType::INT16:
+      type = dType::INT32;
+      break;
+    case dType::UINT8:
+    case dType::UINT16:
+      type = dType::UINT32;
+      break;
+    default:
+      break;
+  }
+  tier0::create_like(src, dst, type);
   const KernelInfo    kernel = { "multiply_image_and_position", kernel::multiply_image_and_position };
   const ParameterList params = { { "src", src }, { "dst", dst }, { "index", dimension } };
   const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
