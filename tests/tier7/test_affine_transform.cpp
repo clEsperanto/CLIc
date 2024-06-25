d8a931d8a34907a71961aad29805da5f789ae53d
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-06-25 10:11:17 +0200

    add interpolation test (should fail)

--- tests/tier7/test_affine_transform.cpp
+++ tests/tier7/test_affine_transform.cpp
@@ -34,6 +34,36 @@ TEST_P(TestAffineTransform, affineTransform)
   }
 }
 
+
+TEST_P(TestAffineTransform, affineTransformInterpolate)
+{
+  const std::array<float, 5 * 5 * 1> input = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  };
+  const std::array<float, 5 * 5 * 1> valid = { 0,   0, 0, 0, 0, 0, 0.5, 0.5, 0, 0, 0, 0, 0.5,
+                                               0.5, 0, 0, 0, 0, 0, 0,   0,   0, 0, 0, 0 };
+  std::array<float, 5 * 5 * 1>       output;
+
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "all");
+  device->setWaitToFinish(true);
+
+  auto gpu_input = cle::Array::create(5, 5, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  gpu_input->write(input.data());
+
+  std::vector<float> matrix = { 1, 0, -0.5, 0, 1, -0.5, 0, 0, 1 };
+  auto               gpu_output = cle::tier7::affine_transform_func(device, gpu_input, nullptr, &matrix, true, false);
+
+  cle::print<float>(gpu_output);
+
+  gpu_output->read(output.data());
+  for (int i = 0; i < output.size(); i++)
+  {
+    EXPECT_EQ(output[i], valid[i]);
+  }
+}
+
 std::vector<std::string>
 getParameters()
 {
