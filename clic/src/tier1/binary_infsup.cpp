7937abfe090be2588d8e4e96505adb33c4b7a2e3
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-06-26 18:30:56 +0200

    fix sup/inf kernel and add tets

--- clic/src/tier1/binary_infsup.cpp
+++ clic/src/tier1/binary_infsup.cpp
@@ -3,7 +3,8 @@
 
 #include "utils.hpp"
 
-#include "cle_inferior_superior.h"
+#include "cle_inferior_superior_2d.h"
+#include "cle_inferior_superior_3d.h"
 
 namespace cle::tier1
 {
@@ -12,7 +13,8 @@ auto
 binary_infsup_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst) -> Array::Pointer
 {
   tier0::create_like(src, dst);
-  const KernelInfo    kernel = { "inferior_superior", kernel::inferior_superior };
+  const KernelInfo    kernel = src->depth() > 1 ? KernelInfo{ "inferior_superior", kernel::inferior_superior_3d }
+                                                : KernelInfo{ "inferior_superior", kernel::inferior_superior_2d };
   const ParameterList params = { { "src", src }, { "dst", dst } };
   const RangeArray    range = { src->width(), src->height(), src->depth() };
   execute(device, kernel, params, range);
--- clic/src/tier1/binary_supinf.cpp
+++ clic/src/tier1/binary_supinf.cpp
@@ -3,7 +3,8 @@
 
 #include "utils.hpp"
 
-#include "cle_superior_inferior.h"
+#include "cle_superior_inferior_2d.h"
+#include "cle_superior_inferior_3d.h"
 
 namespace cle::tier1
 {
@@ -12,7 +13,8 @@ auto
 binary_supinf_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst) -> Array::Pointer
 {
   tier0::create_like(src, dst);
-  const KernelInfo    kernel = { "superior_inferior", kernel::superior_inferior };
+  const KernelInfo    kernel = src->depth() > 1 ? KernelInfo{ "superior_inferior", kernel::superior_inferior_3d }
+                                                : KernelInfo{ "superior_inferior", kernel::superior_inferior_2d };
   const ParameterList params = { { "src", src }, { "dst", dst } };
   const RangeArray    range = { src->width(), src->height(), src->depth() };
   execute(device, kernel, params, range);
