2c99b17bf0cb11cbfe650457ebfa895e9843194e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-07-11 18:00:30 +0200

    convert src to binary if needed

--- clic/src/tier1/binary_infsup.cpp
+++ clic/src/tier1/binary_infsup.cpp
@@ -12,16 +12,23 @@ namespace cle::tier1
 auto
 binary_infsup_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst) -> Array::Pointer
 {
+  cle::Array::Pointer in = nullptr;
   if (src->dtype() != dType::BINARY)
   {
     std::cerr << "Warning: Source image of binary_infsup expected to be binary, " << src->dtype() << " given."
               << std::endl;
+    tier0::create_like(src, in, dType::BINARY);
+    tier1::copy_func(device, src, in);
+  }
+  else
+  {
+    in = src;
   }
   tier0::create_like(src, dst, dType::BINARY);
-  const KernelInfo    kernel = src->depth() > 1 ? KernelInfo{ "inferior_superior", kernel::inferior_superior_3d }
-                                                : KernelInfo{ "inferior_superior", kernel::inferior_superior_2d };
-  const ParameterList params = { { "src", src }, { "dst", dst } };
-  const RangeArray    range = { src->width(), src->height(), src->depth() };
+  const KernelInfo    kernel = in->depth() > 1 ? KernelInfo{ "inferior_superior", kernel::inferior_superior_3d }
+                                               : KernelInfo{ "inferior_superior", kernel::inferior_superior_2d };
+  const ParameterList params = { { "src", in }, { "dst", dst } };
+  const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
   execute(device, kernel, params, range);
   return dst;
 }
--- clic/src/tier1/binary_supinf.cpp
+++ clic/src/tier1/binary_supinf.cpp
@@ -12,17 +12,23 @@ namespace cle::tier1
 auto
 binary_supinf_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst) -> Array::Pointer
 {
+  cle::Array::Pointer in = nullptr;
   if (src->dtype() != dType::BINARY)
   {
     std::cerr << "Warning: Source image of binary_supinf expected to be binary, " << src->dtype() << " given."
               << std::endl;
+    tier0::create_like(src, in, dType::BINARY);
+    tier1::copy_func(device, src, in);
   }
-
+  else
+  {
+    in = src;
+  }
   tier0::create_like(src, dst, dType::BINARY);
-  const KernelInfo    kernel = src->depth() > 1 ? KernelInfo{ "superior_inferior", kernel::superior_inferior_3d }
-                                                : KernelInfo{ "superior_inferior", kernel::superior_inferior_2d };
-  const ParameterList params = { { "src", src }, { "dst", dst } };
-  const RangeArray    range = { src->width(), src->height(), src->depth() };
+  const KernelInfo    kernel = in->depth() > 1 ? KernelInfo{ "superior_inferior", kernel::superior_inferior_3d }
+                                               : KernelInfo{ "superior_inferior", kernel::superior_inferior_2d };
+  const ParameterList params = { { "src", in }, { "dst", dst } };
+  const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
   execute(device, kernel, params, range);
   return dst;
 }
