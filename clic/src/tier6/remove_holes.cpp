4eadd5b9b6229e77521255ed4df0241242dfa3ba
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-09-06 17:59:41 +0200

    fix merge error

--- clic/src/tier6/remove_holes.cpp
+++ clic/src/tier6/remove_holes.cpp
@@ -19,8 +19,8 @@ fill_holes_func(const Device::Pointer & device, const Array::Pointer & src, Arra
   auto binary = tier1::greater_constant_func(device, src, nullptr, 0);
   auto inverted = tier1::binary_not_func(device, binary, nullptr);
 
-  tier5::remove_small_labels_func(device, labels, dst, max_size);
   auto labels = tier5::connected_component_labeling_func(device, inverted, nullptr, "box");
+  tier5::remove_small_labels_func(device, labels, dst, max_size);
 
   // invert the filtered image
   tier1::greater_constant_func(device, dst, binary, 0);
