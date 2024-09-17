d90255b103dec5d778232882d2a8f9cc4c0c9546
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-09-17 10:15:41 +0300

    fix statistic function

--- clic/src/tier3/statistics_of_labelled_pixels.cpp
+++ clic/src/tier3/statistics_of_labelled_pixels.cpp
@@ -7,22 +7,32 @@ namespace cle::tier3
 
 auto
 statistics_of_labelled_pixels_func(const Device::Pointer & device,
-                                   const Array::Pointer &  src,
-                                   Array::Pointer          intensity,
-                                   bool                    withBG) -> StatisticsMap
+                                   const Array::Pointer &  label,
+                                   Array::Pointer          intensity) -> StatisticsMap
 {
-  // create intensity if not set
   if (intensity == nullptr)
   {
     // std::cerr << "Warning: no intensity was provided. Pixels intensity will be set to their label value." <<
     // std::endl;
-    tier0::create_like(src, intensity, dType::FLOAT);
+    tier0::create_like(label, intensity, dType::FLOAT);
     intensity->fill(0);
-    // tier1::copy_func(device, label, intensity);
   }
 
-  size_t offset = (withBG) ? 0 : 1;
-  return compute_statistics_per_labels(device, src, intensity, offset);
+  return compute_statistics_per_labels(device, label, intensity);
+}
+
+
+auto
+statistics_of_background_and_labelled_pixels_func(const Device::Pointer & device,
+                                                  const Array::Pointer &  label,
+                                                  const Array::Pointer &  intensity) -> StatisticsMap
+{
+  auto               temp = tier1::add_image_and_scalar_func(device, label, nullptr, 1);
+  auto               props = tier3::statistics_of_labelled_pixels_func(device, temp, intensity);
+  std::vector<float> labels = props["label"];
+  std::transform(labels.begin(), labels.end(), labels.begin(), [](float label) { return label - 1; });
+  props["label"] = labels;
+  return props;
 }
 
 
