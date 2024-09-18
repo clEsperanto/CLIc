496f13b3e4cee8a19baa25a59c2974070afb9dae
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-09-18 17:17:25 +0300

    move maps into one cpp file

--- clic/src/tier4/parametrics_map.cpp
+++ clic/src/tier4/parametrics_map.cpp
@@ -12,6 +12,28 @@ namespace cle::tier4
 {
 
 auto
+label_pixel_count_map_func(const Device::Pointer & device,
+                           const Array::Pointer &  src,
+                           Array::Pointer          dst) -> Array::Pointer
+{
+  tier0::create_like(src, dst, dType::FLOAT);
+  auto props = tier3::statistics_of_labelled_pixels_func(device, src, nullptr);
+
+  for (auto && i : props["area"])
+  {
+    std::cout << i << std::endl;
+  }
+
+
+  auto values = cle::Array::create(props["area"].size(), 1, 1, 1, dType::FLOAT, mType::BUFFER, device);
+  values->writeFrom(props["area"].data());
+
+  cle::print<float>(values, "values");
+  tier1::set_column_func(device, values, 0, 0);
+  return tier1::replace_values_func(device, src, values, dst);
+}
+
+auto
 extension_ratio_map_func(const Device::Pointer & device,
                          const Array::Pointer &  src,
                          Array::Pointer          dst) -> Array::Pointer
