46eac1c79dcaace17dd96178692ca17b8fbe82a1
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-10-14 15:47:49 +0200

    add tests low value

--- tests/tier4/test_threshold_otsu.cpp
+++ tests/tier4/test_threshold_otsu.cpp
@@ -10,6 +10,12 @@ protected:
   const std::array<float, 3 * 2 * 2>   input = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }; // 6.0058594 skimage
   const std::array<uint8_t, 3 * 2 * 2> valid = { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1 };    // 6.00392 clic
   std::array<uint8_t, 3 * 2 * 2>       output;
+
+
+
+  const std::array<float, 3 * 3 * 1>   input_low = { 0, 0, 0, 0, 0.003, 0, 0, 0, 0}; 
+  const std::array<uint8_t, 3 * 3 * 1> valid_low = { 0, 0, 0, 0, 1, 0, 0, 0, 0 };    
+  std::array<uint8_t, 3 * 3 * 1>       output_low;
 };
 
 TEST_P(TestThresholdOtsu, execute)
@@ -28,6 +34,25 @@ TEST_P(TestThresholdOtsu, execute)
   for (int i = 0; i < output.size(); i++)
   {
     EXPECT_EQ(output[i], valid[i]);
+  }
+}
+
+TEST_P(TestThresholdOtsu, lowIntensity)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "all");
+  device->setWaitToFinish(true);
+
+  auto gpu_input = cle::Array::create(3, 3, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(input_low.data());
+
+  auto gpu_output = cle::tier4::threshold_otsu_func(device, gpu_input, nullptr);
+
+  gpu_output->readTo(output_low.data());
+  for (int i = 0; i < output_low.size(); i++)
+  {
+    EXPECT_EQ(output_low[i], valid_low[i]);
   }
 }
 
