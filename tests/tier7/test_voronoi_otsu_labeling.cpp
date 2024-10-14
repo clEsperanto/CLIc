cc520be2c972fdb67267f7410c1f9de2f60437d9
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-10-14 17:12:56 +0200

    updata tests

--- tests/tier7/test_voronoi_otsu_labeling.cpp
+++ tests/tier7/test_voronoi_otsu_labeling.cpp
@@ -6,19 +6,29 @@
 class TestVoronoiOtsuLabeling : public ::testing::TestWithParam<std::string>
 {
 protected:
-  const std::array<float, 7 * 7 * 3> input = {
-    0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
-    1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 3, 1, 0, 0, 0, 1, 5, 8, 2, 0, 0, 0, 1, 1, 5, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 5, 1, 1, 0, 0, 0, 2, 8, 5, 1, 0, 0, 0, 1, 3, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1,
-    1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0
+  const std::array<float, 6 * 7 * 1> input = {
+
+            0, 0, 1, 1, 0, 0,
+            0, 1, 8, 9, 1, 0,
+            0, 1, 7, 6, 1, 0,
+            0, 0, 1, 1, 1, 0,
+            0, 0, 1, 1, 1, 0,
+            0, 0, 1, 8, 7, 1,
+            0, 0, 1, 1, 1, 0
+
+
+
   };
-  const std::array<uint32_t, 7 * 7 * 3> valid = {
-    0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 0, 0, 0, 2, 2,
-    2, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 2, 1, 1, 1, 1, 2, 2, 2, 2,
-    1, 1, 1, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1,
-    1, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0
+  const std::array<uint32_t, 6 * 7 * 1> valid = {
+            0, 0, 1, 1, 0, 0,
+            0, 1, 1, 1, 1, 0,
+            0, 1, 1, 1, 1, 0,
+            0, 0, 1, 2, 2, 0,
+            0, 0, 0, 2, 2, 0,
+            0, 0, 2, 2, 2, 0,
+            0, 0, 0, 2, 2, 0
   };
-  std::array<uint32_t, 7 * 7 * 3> output;
+  std::array<uint32_t, 6 * 7 * 1> output;
 };
 
 TEST_P(TestVoronoiOtsuLabeling, execute)
@@ -28,10 +38,10 @@ TEST_P(TestVoronoiOtsuLabeling, execute)
   auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "all");
   device->setWaitToFinish(true);
 
-  auto gpu_input = cle::Array::create(7, 7, 3, 3, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  auto gpu_input = cle::Array::create(6, 7, 1, 1, cle::dType::FLOAT, cle::mType::BUFFER, device);
   gpu_input->writeFrom(input.data());
 
-  auto gpu_output = cle::tier7::voronoi_otsu_labeling_func(device, gpu_input, nullptr, 0, 1);
+  auto gpu_output = cle::tier7::voronoi_otsu_labeling_func(device, gpu_input, nullptr, 1, 1);
 
   gpu_output->readTo(output.data());
   for (int i = 0; i < output.size(); i++)
