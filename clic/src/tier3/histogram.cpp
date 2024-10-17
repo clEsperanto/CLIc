732b9b4ce3f8bc5f8234201a5f1b7ea229bc809f
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-10-17 10:11:42 +0200

    rename parameters in histogram

--- clic/src/tier3/histogram.cpp
+++ clic/src/tier3/histogram.cpp
@@ -14,23 +14,23 @@ auto
 histogram_func(const Device::Pointer & device,
                const Array::Pointer &  src,
                Array::Pointer          dst,
-               int                     nbins,
-               float                   min,
-               float                   max) -> Array::Pointer
+               int                     num_bins,
+               float                   minimum_intensity,
+               float                   maximum_intensity) -> Array::Pointer
 {
-  tier0::create_vector(src, dst, nbins, dType::INDEX);
+  tier0::create_vector(src, dst, num_bins, dType::INDEX);
   size_t number_of_partial_histograms = src->height();
   auto   partial_hist =
-    Array::create(nbins, 1, number_of_partial_histograms, 3, dType::INDEX, src->mtype(), src->device());
-  if (std::isnan(max) || std::isnan(max))
+    Array::create(num_bins, 1, number_of_partial_histograms, 3, dType::INDEX, src->mtype(), src->device());
+  if (std::isnan(maximum_intensity) || std::isnan(maximum_intensity))
   {
-    min = tier2::minimum_of_all_pixels_func(device, src);
-    max = tier2::maximum_of_all_pixels_func(device, src);
+    minimum_intensity = tier2::minimum_of_all_pixels_func(device, src);
+    maximum_intensity = tier2::maximum_of_all_pixels_func(device, src);
   }
   const KernelInfo    kernel = { "histogram", kernel::histogram };
-  const ParameterList params = { { "src", src },       { "dst", partial_hist }, { "minimum", min },  { "maximum", max },
+  const ParameterList params = { { "src", src },       { "dst", partial_hist }, { "minimum", minimum_intensity },  { "maximum", maximum_intensity },
                                  { "step_size_x", 1 }, { "step_size_y", 1 },    { "step_size_z", 1 } };
-  const ConstantList  consts = { { "NUMBER_OF_HISTOGRAM_BINS", nbins } };
+  const ConstantList  consts = { { "NUMBER_OF_HISTOGRAM_BINS", num_bins } };
   const RangeArray    range = { number_of_partial_histograms, 1, 1 };
   execute(device, kernel, params, range, consts);
   return tier1::sum_z_projection_func(device, partial_hist, dst);
