863e4a50e481eacd4f48326d88f0d1f5b54ed9ae
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-11-12 12:48:21 +0100

    update divide image

--- clic/src/tier1/divide_images.cpp
+++ clic/src/tier1/divide_images.cpp
@@ -10,13 +10,13 @@ namespace cle::tier1
 
 auto
 divide_images_func(const Device::Pointer & device,
-                   const Array::Pointer &  src0,
-                   const Array::Pointer &  src1,
+                   const Array::Pointer &  dividend,
+                   const Array::Pointer &  divisor,
                    Array::Pointer          dst) -> Array::Pointer
 {
-  tier0::create_like(src0, dst);
+  tier0::create_like(dividend, dst);
   const KernelInfo    kernel = { "divide_images", kernel::divide_images };
-  const ParameterList params = { { "src0", src0 }, { "src1", src1 }, { "dst", dst } };
+  const ParameterList params = { { "src0", dividend }, { "src1", divisor }, { "dst", dst } };
   const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
   execute(device, kernel, params, range);
   return dst;
