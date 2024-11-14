34a629509720dbdd997ec584fc2d0539af3ff90d
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-11-14 11:35:14 +0100

    fix typo

--- clic/src/tier1/set_image_borders.cpp
+++ clic/src/tier1/set_image_borders.cpp
@@ -12,7 +12,7 @@ auto
 set_image_borders_func(const Device::Pointer & device, const Array::Pointer & src, float value) -> Array::Pointer
 {
   const KernelInfo    kernel = { "set_image_borders", kernel::set_image_borders };
-  const ParameterList params = { { "dst", src }, { "scalar", value } };
+  const ParameterList params = { { "src", src }, { "scalar", value } };
   const RangeArray    range = { src->width(), src->height(), src->depth() };
   execute(device, kernel, params, range);
   return src;
