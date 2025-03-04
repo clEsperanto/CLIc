21b18c4589b3cce8f848dc95c3054a41281f41a1
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-03-04 18:06:16 +0100

    add test (WIP)

--- tests/tier1/test_fft.cpp
+++ tests/tier1/test_fft.cpp
@@ -7,8 +7,18 @@ class TestFFT : public ::testing::TestWithParam<std::string>
 {
 protected:
   std::array<float, 10 * 5 * 1> input_2 = {
-    1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+    1,  2,  3,  4,  5,  6,  7,  8,  9, 10,   
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,  
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,  
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+  };
+  std::array<float, 10 * 5 * 1> input_3 = {
+    10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0,0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0,0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,0,
   };
 };
 
@@ -17,7 +27,7 @@ TEST_P(TestFFT, executeCLFFT)
   std::string param = GetParam();
   cle::BackendManager::getInstance().setBackend(param);
 
-  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "cpu");
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
   device->setWaitToFinish(true);
 
   auto gpu_input = cle::Array::create(10, 5, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
@@ -53,7 +63,7 @@ TEST_P(TestFFT, executeVKFFT)
   std::string param = GetParam();
   cle::BackendManager::getInstance().setBackend(param);
 
-  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "cpu");
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
   device->setWaitToFinish(true);
 
   auto gpu_input = cle::Array::create(10, 5, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
@@ -69,6 +79,87 @@ TEST_P(TestFFT, executeVKFFT)
   cle::fft::performIFFT(output, gpu_final);
   cle::print<float>(gpu_final, "output real");
 }
+
+
+TEST_P(TestFFT, executeConvolution)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
+
+  auto gpu_input = cle::Array::create(10, 5, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(input_2.data());
+
+  auto gpu_psf = cle::Array::create(gpu_input);
+  gpu_psf->writeFrom(input_3.data());
+
+  cle::print<float>(gpu_input, "input");
+  cle::print<float>(gpu_psf, "kernel");
+
+  auto gpu_final = cle::fft::performConvolution(gpu_input, gpu_psf, nullptr, false);
+  
+  cle::print<float>(gpu_final, "output");
+}
+
+
+TEST_P(TestFFT, executeConvolutionCorrelated)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
+
+  auto gpu_input = cle::Array::create(10, 5, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(input_2.data());
+
+  auto gpu_psf = cle::Array::create(gpu_input);
+  gpu_psf->writeFrom(input_3.data());
+
+  auto gpu_final = cle::fft::performConvolution(gpu_input, gpu_psf, nullptr, true);
+  
+  cle::print<float>(gpu_final, "output");
+}
+
+
+TEST_P(TestFFT, executeDeconvolution)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
+
+
+  std::array<float, 9 * 1 * 1> input = {
+    0, 2, 2, 0, 2, 2, 2, 0, 0
+  };
+  std::array<float, 9 * 1 * 1> psf = {
+    2, 0, 0, 0, 0, 0, 0, 0, 0 
+  };
+
+
+  auto gpu_input = cle::Array::create(9, 1, 1, 1, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(input.data());
+
+  auto gpu_psf = cle::Array::create(gpu_input);
+  gpu_psf->writeFrom(psf.data());
+
+  auto gpu_estimate = cle::Array::create(gpu_input);
+  gpu_estimate->writeFrom(input.data());
+
+  auto gpu_normal = cle::Array::create(gpu_input);
+  gpu_normal->fill(1);
+
+  cle::fft::performDeconvolution(gpu_input, gpu_psf, gpu_normal, gpu_estimate, 1, 0);
+  
+  cle::print<float>(gpu_estimate, "output");
+}
+
+
+
 
 std::vector<std::string>
 getParameters()
