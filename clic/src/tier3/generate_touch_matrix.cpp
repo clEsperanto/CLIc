4be037cf81b4f91a39b02d4dee4e4ab45aaeb260
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-06-23 12:55:57 +0200

    update generate touching matrix function

--- clic/src/tier3/generate_touch_matrix.cpp
+++ clic/src/tier3/generate_touch_matrix.cpp
@@ -11,20 +11,21 @@ namespace cle::tier3
 {
 
 auto
-generate_touch_matrix_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst)
-  -> Array::Pointer
+generate_touch_matrix_func(const Device::Pointer & device,
+                           const Array::Pointer &  src,
+                           Array::Pointer          dst_matrix) -> Array::Pointer
 {
-  if (dst == nullptr)
+  if (dst_matrix == nullptr)
   {
     auto max_label = tier2::maximum_of_all_pixels_func(device, src) + 1;
-    tier0::create_dst(src, dst, max_label, max_label, 1, dType::INDEX);
+    tier0::create_dst(src, dst_matrix, max_label, max_label, 1, dType::INDEX);
   }
-  dst->fill(0);
+  dst_matrix->fill(0);
   const KernelInfo    kernel = { "generate_touch_matrix", kernel::generate_touch_matrix };
-  const ParameterList params = { { "src", src }, { "dst", dst } };
+  const ParameterList params = { { "src", src }, { "dst", dst_matrix } };
   const RangeArray    range = { src->width(), src->height(), src->depth() };
   execute(device, kernel, params, range);
-  return dst;
+  return dst_matrix;
 }
 
 } // namespace cle::tier3
