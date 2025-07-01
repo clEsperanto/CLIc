2a227f21b05ca5082e41d9b6ce329650f29fe09e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-07-01 13:53:44 +0200

    add tests (WIP)

--- tests/tier1/test_gaussian_derivative.cpp
+++ tests/tier1/test_gaussian_derivative.cpp
@@ -9,18 +9,19 @@ protected:
   std::array<int32_t, 5 * 5 * 1> input;
 
   std::array<float, 5 * 5 * 1> output;
-  std::array<float, 5 * 5 * 1> valid = { 0.2915041745F, 1.306431174F, 2.153940439F, 1.306431174F, 0.2915041745F,
-                                         1.306431055F,  5.855018139F, 9.653291702F, 5.855018139F, 1.306431055F,
-                                         2.153940678F,  9.653292656F, 15.91558743F, 9.653292656F, 2.153940678F,
-                                         1.306431055F,  5.855018139F, 9.653291702F, 5.855018139F, 1.306431055F,
-                                         0.2915041745F, 1.306431174F, 2.153940439F, 1.306431174F, 0.2915041745F };
+  std::array<float, 5 * 5 * 1> valid = {   
+0.000467901 ,0.00469903 ,0 ,-0.00469903 ,-0.000467901 ,
+0.00939806 ,0.0943825 ,0 ,-0.0943825 ,-0.00939806 ,
+0.0255466 ,0.256558 ,0 ,-0.256558 ,-0.0255466 ,
+0.00939806 ,0.0943825 ,0 ,-0.0943825 ,-0.00939806 ,
+0.000467901 ,0.00469903 ,0 ,-0.00469903 ,-0.000467901  };
 
   virtual void
   SetUp()
   {
     std::fill(input.begin(), input.end(), 0);
     const int center = (5 / 2) + (5 / 2) * 5;
-    input[center] = 100;
+    input[center] = 1;
   }
 };
 
@@ -36,13 +37,11 @@ TEST_P(TestGaussianDerivative, execute)
 
   auto gpu_output = cle::tier1::gaussian_derivative_func(device, gpu_input, nullptr, 1, 1, 1, 1, 0, 0);
 
-  cle::print<float>(gpu_output, "output");
-
-  // gpu_output->readTo(output.data());
-  // for (int i = 0; i < output.size(); i++)
-  // {
-  //   EXPECT_NEAR(output[i], valid[i], 0.0001);
-  // }
+  gpu_output->readTo(output.data());
+  for (int i = 0; i < output.size(); i++)
+  {
+    EXPECT_NEAR(output[i], valid[i], 0.0001);
+  }
 }
 
 std::vector<std::string>
