f1e57aa17bcd095ee8ae6c4f7c610ed04c2389e3
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-07-02 13:03:52 +0200

    update sato filter

--- clic/src/tier3/sato_filter.cpp
+++ clic/src/tier3/sato_filter.cpp
@@ -12,13 +12,16 @@ auto
 sato_filter_func(const Device::Pointer & device,
            const Array::Pointer &  src,
            Array::Pointer          dst,
-           int                   sigma_minimum,
-           int                   sigma_maximum,
-           int                   sigma_step) -> Array::Pointer
+           float                   sigma_minimum,
+           float                   sigma_maximum,
+           float                   sigma_step) -> Array::Pointer
 {
   tier0::create_like(src, dst);
+  dst->fill(0.0f);
+
   auto is_3d = (src->depth() > 1);
 
+  Array::Pointer middle_eigenvalue = nullptr;
   auto large_eigenvalue = Array::create(src->width(),
                                        src->height(),
                                        src->depth(),
@@ -26,7 +29,6 @@ sato_filter_func(const Device::Pointer & device,
                                        dType::FLOAT,
                                        mType::BUFFER,
                                        device);
-  Array::Pointer middle_eigenvalue = nullptr;
   if (is_3d)
   {
     middle_eigenvalue = Array::create(src->width(),
@@ -38,14 +40,11 @@ sato_filter_func(const Device::Pointer & device,
                                        device);
   }
 
-  float max = 0.0f;
-  float sigma_squared = 0.0f;
-  for(int sigma = sigma_minimum; sigma < sigma_maximum; sigma += sigma_step)
+  for(int sigma = static_cast<int>(sigma_minimum); sigma < static_cast<int>(sigma_maximum); sigma += static_cast<int>(sigma_step) )
   {
-    sigma_squared = static_cast<float>(sigma * sigma);
     tier2::hessian_gaussian_eigenvalues_func(device, src, nullptr, middle_eigenvalue, large_eigenvalue, sigma);
-
-    max = tier2::maximum_of_all_pixels_func(device, large_eigenvalue);
+        
+    float max = tier2::maximum_of_all_pixels_func(device, large_eigenvalue);
     tier2::clip_func(device, large_eigenvalue, large_eigenvalue, 0.0f, max);
     
     auto mean_eigenvalue = large_eigenvalue;
@@ -56,7 +55,10 @@ sato_filter_func(const Device::Pointer & device,
       mean_eigenvalue = tier1::multiply_images_func(device, middle_eigenvalue, large_eigenvalue, nullptr);
       tier1::power_func(device, mean_eigenvalue, nullptr, 0.5f);
     }
+
+    float sigma_squared = static_cast<float>(sigma * sigma);
     auto value = tier1::multiply_image_and_scalar_func(device, mean_eigenvalue, nullptr, sigma_squared);
+
     tier1::maximum_images_func(device, value, dst, dst);
   }
 
@@ -64,3 +66,4 @@ sato_filter_func(const Device::Pointer & device,
 }
 
 } // namespace cle::tier3
+
