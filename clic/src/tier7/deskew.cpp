422f586c85d768161777d7c099ec63fc3baf166e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-10-23 16:17:54 +0200

    add deskew operation to tiers

---
+++ clic/src/tier7/deskew.cpp
@@ -0,0 +1,51 @@
+#include "tier0.hpp"
+#include "tier1.hpp"
+#include "tier2.hpp"
+#include "tier3.hpp"
+#include "tier4.hpp"
+#include "tier5.hpp"
+#include "tier6.hpp"
+#include "tier7.hpp"
+
+#include "transform.hpp"
+#include "utils.hpp"
+
+namespace cle::tier7
+{
+
+auto
+deskew_x_func(const Device::Pointer & device,
+              const Array::Pointer &  src,
+              Array::Pointer          dst,
+              float                   angle,
+              float                   voxel_size_x,
+              float                   voxel_size_y,
+              float                   voxel_size_z,
+              float                   scale_factor) -> Array::Pointer
+{
+    auto transform = AffineTransform();
+    transform.deskew_x(angle, voxel_size_x, voxel_size_y, voxel_size_z, scale_factor);
+
+    return apply_affine_transform_deskew_3d(
+        src, dst, transform, angle, voxel_size_x, voxel_size_y, voxel_size_z, 0, true);
+}
+
+
+auto
+deskew_y_func(const Device::Pointer & device,
+              const Array::Pointer &  src,
+              Array::Pointer          dst,
+              float                   angle,
+              float                   voxel_size_x,
+              float                   voxel_size_y,
+              float                   voxel_size_z,
+              float                   scale_factor) -> Array::Pointer
+{
+    auto transform = AffineTransform();
+    transform.deskew_y(angle, voxel_size_x, voxel_size_y, voxel_size_z, scale_factor);
+
+    return apply_affine_transform_deskew_3d(
+        src, dst, transform, angle, voxel_size_x, voxel_size_y, voxel_size_z, 1, true);
+}
+
+} // namespace cle::tier7
