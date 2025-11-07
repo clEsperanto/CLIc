8e058a3492ba9556c11762bee246e73056d29eb8
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-11-07 11:27:32 +0100

    fix var name

--- clic/src/tier2/proximal_neighbor_matrix.cpp
+++ clic/src/tier2/proximal_neighbor_matrix.cpp
@@ -19,13 +19,15 @@ generate_proximal_neighbors_matrix_func(const Device::Pointer & device,
   min_distance = std::max(min_distance, 0.0f);
   max_distance = (max_distance < 0) ? std::numeric_limits<float>::max() : max_distance;
 
-  auto above_min_distance = tier1::greater_or_equal_constant_func(distance_matrix, nullptr, min_distance) auto below_max_distance =
-    tier1::smaller_or_equal_constant_func(distance_matrix, nullptr, max_distance)
+  auto above_min_distance = tier1::greater_or_equal_constant_func(src_matrix, nullptr, min_distance);
+  auto below_max_distance = tier1::smaller_or_equal_constant_func(src_matrix, nullptr, max_distance);
 
-      tier1::binary_and_func(above_min_distance, below_max_distance, dst_matrix) tier1::set_where_x_equals_y_func(dst_matrix, 0)
-        tier1::set_row_func(dst_matrix, 0, 0) tier1::set_column_func(dst_matrix, 0, 0)
+  tier1::binary_and_func(above_min_distance, below_max_distance, dst_matrix);
+  tier1::set_where_x_equals_y_func(dst_matrix, 0);
+  tier1::set_row_func(dst_matrix, 0, 0);
+  tier1::set_column_func(dst_matrix, 0, 0);
 
-          return dst_matrix
+  return dst_matrix;
 }
 
 
