45b1a62415ec31e16dfd987960bb5706c5a81d4e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-11-13 14:05:27 +0100

    add normalize + test

---
+++ tests/tier5/test_normalization.cpp
@@ -0,0 +1,48 @@
+#include "cle.hpp"
+
+#include <array>
+#include <gtest/gtest.h>
+
+class TestNormalize : public ::testing::TestWithParam<std::string>
+{
+protected:
+
+  std::array<float, 11 * 1 * 1> input = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  std::array<float, 11 * 1 * 1> valid = { 0, 0, 0.125, 0.25, 0.375, 0.5 , 0.625, 0.75, 0.875, 1, 1};
+  std::array<float, 11 * 1 * 1> output;
+};
+
+TEST_P(TestNormalize, execute)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
+
+  auto gpu_input = cle::Array::create(11, 1, 1, 1, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(input.data());
+
+  auto gpu_output = cle::tier5::normalize_func(device, gpu_input, nullptr, 10, 90);
+
+  gpu_output->readTo(output.data());
+  for (int i = 0; i < output.size(); i++)
+  {
+    EXPECT_NEAR(output[i], valid[i], 0.01);
+  }
+
+}
+
+std::vector<std::string>
+getParameters()
+{
+  std::vector<std::string> parameters;
+#if USE_OPENCL
+  parameters.push_back("opencl");
+#endif
+#if USE_CUDA
+  parameters.push_back("cuda");
+#endif
+  return parameters;
+}
+
+INSTANTIATE_TEST_SUITE_P(InstantiationName, TestNormalize, ::testing::ValuesIn(getParameters()));
