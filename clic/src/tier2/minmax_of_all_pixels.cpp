9cc8eac81efb9b5b50a12c1de7c83037fee0f2c7
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-11-29 12:20:11 +0100

    refactor code

--- clic/src/tier2/minmax_of_all_pixels.cpp
+++ clic/src/tier2/minmax_of_all_pixels.cpp
@@ -8,6 +8,29 @@ namespace cle::tier2
 {
 
 auto
+maximum_of_all_pixels_func(const Device::Pointer & device, const Array::Pointer & src) -> float
+{
+  Array::Pointer dst = nullptr;
+  Array::Pointer tmp = src;
+  tier0::create_one(src, dst, dType::FLOAT);
+
+  auto project_if_needed = [&](auto projection_func, int dimension) {
+    if (dimension > 1)
+    {
+      tmp = projection_func(device, tmp, nullptr);
+    }
+  };
+
+  project_if_needed(tier1::maximum_z_projection_func, tmp->depth());
+  project_if_needed(tier1::maximum_y_projection_func, tmp->height());
+  tier1::maximum_x_projection_func(device, tmp, dst);
+
+  float res;
+  dst->readTo(&res);
+  return res;
+}
+
+auto
 minimum_of_all_pixels_func(const Device::Pointer & device, const Array::Pointer & src) -> float
 {
   Array::Pointer dst = nullptr;
