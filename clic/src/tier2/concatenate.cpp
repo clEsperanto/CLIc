3519fa1d96bee74ba8316400ed989a1553dd135a
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-11-29 14:36:28 +0100

    fix segfault in return dst

--- clic/src/tier2/concatenate.cpp
+++ clic/src/tier2/concatenate.cpp
@@ -35,7 +35,7 @@ concatenate(const Device::Pointer & device, const Array::Pointer & src0, const A
       break;
     }
     default:
-      throw std::invalid_argument("concatenate: axis must be 0, 1 or 2");
+    throw std::invalid_argument("concatenate: axis must be 0, 1 or 2");
   }
   return dst;
 }
@@ -44,24 +44,21 @@ auto
 concatenate_along_x_func(const Device::Pointer & device, const Array::Pointer & src0, const Array::Pointer & src1, Array::Pointer dst)
   -> Array::Pointer
 {
-  concatenate(device, src0, src1, dst, 0);
-  return dst;
+  return concatenate(device, src0, src1, dst, 0);
 }
 
 auto
 concatenate_along_y_func(const Device::Pointer & device, const Array::Pointer & src0, const Array::Pointer & src1, Array::Pointer dst)
   -> Array::Pointer
 {
-  concatenate(device, src0, src1, dst, 1);
-  return dst;
+  return concatenate(device, src0, src1, dst, 1);
 }
 
 auto
 concatenate_along_z_func(const Device::Pointer & device, const Array::Pointer & src0, const Array::Pointer & src1, Array::Pointer dst)
   -> Array::Pointer
 {
-  concatenate(device, src0, src1, dst, 2);
-  return dst;
+  return concatenate(device, src0, src1, dst, 2);
 }
 
 } // namespace cle::tier2
--- tests/tier2/test_concatenate.cpp
+++ tests/tier2/test_concatenate.cpp
@@ -11,6 +11,34 @@ protected:
   std::array<float, 2 * 2 * 1> input2 = { 2, 2, 2, 2 };
 };
 
+TEST_P(TestConcatenate, alongX_withDst)
+{
+  std::array<float, 4 * 2 * 1> valid = { 1, 1, 2, 2, 1, 1, 2, 2 };
+
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
+
+  auto gpu_input1 = cle::Array::create(2, 2, 1, 3, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  auto gpu_input2 = cle::Array::create(gpu_input1);
+  gpu_input1->writeFrom(input1.data());
+  gpu_input2->writeFrom(input2.data());
+
+
+  auto gpu_output = cle::Array::create(4, 2, 1, 3, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  cle::tier2::concatenate_along_x_func(device, gpu_input1, gpu_input2, gpu_output);
+
+  gpu_output->readTo(output.data());
+  for (int i = 0; i < output.size(); i++)
+  {
+    EXPECT_EQ(output[i], valid[i]);
+  }
+  EXPECT_EQ(gpu_output->width(), 4);
+  EXPECT_EQ(gpu_output->height(), 2);
+  EXPECT_EQ(gpu_output->depth(), 1);
+}
+
 TEST_P(TestConcatenate, alongX)
 {
   std::array<float, 4 * 2 * 1> valid = { 1, 1, 2, 2, 1, 1, 2, 2 };
