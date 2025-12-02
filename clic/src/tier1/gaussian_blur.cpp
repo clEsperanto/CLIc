229224104eaeb86515730ecec70a4b0bdcd5f950
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-12-02 16:56:08 +0100

    WIP convolution and gaussian blur use shared library optomization

--- clic/src/tier1/gaussian_blur.cpp
+++ clic/src/tier1/gaussian_blur.cpp
@@ -5,9 +5,37 @@
 
 #include "cle_gaussian_blur_separable.h"
 #include "cle_gaussian_derivative_separable.h"
+#include "cle_gaussian_blur_separable_shared.h"
+#include "cle_gaussian_derivative_separable_shared.h"
 
 namespace cle::tier1
 {
+
+// auto
+// gaussian_blur_func(const Device::Pointer & device,
+//                    const Array::Pointer &  src,
+//                    Array::Pointer          dst,
+//                    float                   sigma_x,
+//                    float                   sigma_y,
+//                    float                   sigma_z) -> Array::Pointer
+// {
+//   tier0::create_like(src, dst, dType::FLOAT);
+//   Array::Pointer temp = src;
+//   if (temp->dtype() != dType::FLOAT)
+//   {
+//     temp = Array::create(dst);
+//     tier1::copy_func(device, src, temp);
+//   }
+//   const KernelInfo kernel = { "gaussian_blur_separable", kernel::gaussian_blur_separable };
+//   execute_separable(device,
+//                     kernel,
+//                     temp,
+//                     dst,
+//                     { sigma_x, sigma_y, sigma_z },
+//                     { sigma2kernelsize(sigma_x), sigma2kernelsize(sigma_y), sigma2kernelsize(sigma_z) },
+//                     { 0, 0, 0 });
+//   return dst;
+// }
 
 auto
 gaussian_blur_func(const Device::Pointer & device,
@@ -24,7 +52,8 @@ gaussian_blur_func(const Device::Pointer & device,
     temp = Array::create(dst);
     tier1::copy_func(device, src, temp);
   }
-  const KernelInfo kernel = { "gaussian_blur_separable", kernel::gaussian_blur_separable };
+  try {
+  const KernelInfo kernel = { "gaussian_blur_separable_shared", kernel::gaussian_blur_separable_shared };
   execute_separable(device,
                     kernel,
                     temp,
@@ -32,8 +61,51 @@ gaussian_blur_func(const Device::Pointer & device,
                     { sigma_x, sigma_y, sigma_z },
                     { sigma2kernelsize(sigma_x), sigma2kernelsize(sigma_y), sigma2kernelsize(sigma_z) },
                     { 0, 0, 0 });
+  } 
+  catch (const std::runtime_error & e) {
+    std::cerr << e.what() << std::endl;
+    const KernelInfo kernel_global = { "gaussian_blur_separable", kernel::gaussian_blur_separable };
+    execute_separable(device,
+                      kernel_global,
+                      temp,
+                      dst,
+                      { sigma_x, sigma_y, sigma_z },
+                      { sigma2kernelsize(sigma_x), sigma2kernelsize(sigma_y), sigma2kernelsize(sigma_z) },
+                      { 0, 0, 0 });
+  }
   return dst;
 }
+
+// auto
+// gaussian_derivative_func(const Device::Pointer & device,
+//                          const Array::Pointer &  src,
+//                          Array::Pointer          dst,
+//                          float                   sigma_x,
+//                          float                   sigma_y,
+//                          float                   sigma_z,
+//                          int                     order_x,
+//                          int                     order_y,
+//                          int                     order_z) -> Array::Pointer
+// {
+//   tier0::create_like(src, dst, dType::FLOAT);
+//   Array::Pointer temp = src;
+//   if (temp->dtype() != dType::FLOAT)
+//   {
+//     temp = Array::create(dst);
+//     tier1::copy_func(device, src, temp);
+//   }
+
+//   constexpr int        truncate = 8;
+//   std::array<float, 3> sigmas = { std::max({ sigma_x, 0.0f }), std::max({ sigma_y, 0.0f }), std::max({ sigma_z, 0.0f }) };
+//   std::array<int, 3>   radii = { static_cast<int>(truncate * sigmas[0] + 0.5f),
+//                                  static_cast<int>(truncate * sigmas[1] + 0.5f),
+//                                  static_cast<int>(truncate * sigmas[2] + 0.5f) };
+//   std::array<int, 3>   orders = { std::min(order_x, 2), std::min(order_y, 2), std::min(order_z, 2) };
+
+//   const KernelInfo kernel = { "gaussian_derivative_separable", kernel::gaussian_derivative_separable };
+//   execute_separable(device, kernel, temp, dst, sigmas, radii, orders);
+//   return dst;
+// }
 
 auto
 gaussian_derivative_func(const Device::Pointer & device,
@@ -61,9 +133,18 @@ gaussian_derivative_func(const Device::Pointer & device,
                                  static_cast<int>(truncate * sigmas[2] + 0.5f) };
   std::array<int, 3>   orders = { std::min(order_x, 2), std::min(order_y, 2), std::min(order_z, 2) };
 
-  const KernelInfo kernel = { "gaussian_derivative_separable", kernel::gaussian_derivative_separable };
-  execute_separable(device, kernel, temp, dst, sigmas, radii, orders);
+  try {
+    const KernelInfo kernel = { "gaussian_derivative_separable_shared", kernel::gaussian_derivative_separable_shared };
+    execute_separable(device, kernel, temp, dst, sigmas, radii, orders);
+  } 
+  catch (const std::runtime_error & e) 
+  {
+    std::cerr << e.what() << std::endl;
+    const KernelInfo kernel = { "gaussian_derivative_separable", kernel::gaussian_derivative_separable };
+    execute_separable(device, kernel, temp, dst, sigmas, radii, orders);
+  }
   return dst;
 }
+
 
 } // namespace cle::tier1
--- tests/tier1/test_convolve.cpp
+++ tests/tier1/test_convolve.cpp
@@ -19,9 +19,9 @@ TEST_P(TestConvolve, execute)
   auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
   device->setWaitToFinish(true);
 
-  auto gpu_input = cle::Array::create(4, 4, 1, 3, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  auto gpu_input = cle::Array::create(4, 4, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
   gpu_input->writeFrom(input.data());
-  auto gpu_kernel = cle::Array::create(3, 3, 1, 3, cle::dType::FLOAT, cle::mType::BUFFER, device);
+  auto gpu_kernel = cle::Array::create(3, 3, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
   gpu_kernel->writeFrom(kernel.data());
 
   auto gpu_output = cle::tier1::convolve_func(device, gpu_input, gpu_kernel, nullptr);
