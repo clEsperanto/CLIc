ece8bb1e5917ac68ffda307d98f82dbb97039787
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-12-10 18:20:32 +0100

    integrate hardware copy with kernel copy

--- clic/src/tier1/copy.cpp
+++ clic/src/tier1/copy.cpp
@@ -18,10 +18,17 @@ auto
 copy_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst) -> Array::Pointer
 {
   tier0::create_like(src, dst);
-  const KernelInfo    kernel = { "copy", kernel::copy };
-  const ParameterList params = { { "src", src }, { "dst", dst } };
-  const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
-  execute(device, kernel, params, range);
+  if(src->dtype() == dst->dtype())
+  {
+    src->copyTo(dst);
+  }
+  else
+  {
+    const KernelInfo    kernel = { "copy", kernel::copy };
+    const ParameterList params = { { "src", src }, { "dst", dst } };
+    const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
+    execute(device, kernel, params, range);
+  }
   return dst;
 }
 
