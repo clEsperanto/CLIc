8dfd2aaca605ccb35b2bd052ec29ae7fe4a52893
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-12-10 16:19:34 +0100

    reuse unary operation kernel

--- clic/src/tier1/math_trigonometry_ops.cpp
+++ clic/src/tier1/math_trigonometry_ops.cpp
@@ -1,7 +1,7 @@
 #include "tier0.hpp"
 #include "tier1.hpp"
 
-#include "cle_trigonometry_operation.h"
+#include "cle_unary_operation.h"
 #include "utils.hpp"
 
 namespace cle::tier1
@@ -14,7 +14,7 @@ apply_trigonometric_op(const Device::Pointer & device, const Array::Pointer & sr
   -> Array::Pointer
 {
   tier0::create_like(src, dst);
-  const KernelInfo    kernel_info = { "trigonometry_operation", kernel::trigonometry_operation };
+  const KernelInfo    kernel_info = { "unary_operation", kernel::unary_operation };
   const ParameterList params = { { "src", src }, { "dst", dst } };
   const RangeArray    range = { src->width(), src->height(), src->depth() };
   const ConstantList  constants = { { "APPLY_OP(x)", op_define } };
