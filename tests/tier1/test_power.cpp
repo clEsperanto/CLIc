230e36ab0ea4a81ff6fe3123e58c417501f38590
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2026-02-27 11:33:14 +0100

    check if power is correct for int too

--- tests/tier1/test_power.cpp
+++ tests/tier1/test_power.cpp
@@ -7,26 +7,30 @@
 class TestPower : public ::testing::TestWithParam<std::string>
 {
 protected:
-  const float                   value = 4;
+  const float                   value = 5;
   const float                   exp = 3;
-  std::array<float, 10 * 5 * 3> output;
-  std::array<float, 10 * 5 * 3> input;
-  std::array<float, 10 * 5 * 3> valid;
+  std::string backend;
+  cle::Device::Pointer device;
 
   virtual void
   SetUp()
   {
-    std::fill(input.begin(), input.end(), static_cast<float>(value));
-    std::fill(valid.begin(), valid.end(), static_cast<float>(std::pow(value, exp)));
+  std::string backend = GetParam();
+  cle::BackendManager::getInstance().setBackend(backend);
+  device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
   }
 };
 
-TEST_P(TestPower, execute)
+TEST_P(TestPower, execute_w_float)
 {
-  std::string param = GetParam();
-  cle::BackendManager::getInstance().setBackend(param);
-  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
-  device->setWaitToFinish(true);
+
+  std::array<float, 10 * 5 * 3> output;
+  std::array<float, 10 * 5 * 3> input;
+  std::array<float, 10 * 5 * 3> valid;
+
+  std::fill(input.begin(), input.end(), static_cast<float>(value));
+  std::fill(valid.begin(), valid.end(), static_cast<float>(std::pow(value, exp)));
 
   auto gpu_input = cle::Array::create(10, 5, 3, 3, cle::dType::FLOAT, cle::mType::BUFFER, device);
   gpu_input->writeFrom(input.data());
@@ -39,4 +43,27 @@ TEST_P(TestPower, execute)
     EXPECT_EQ(output[i], valid[i]);
   }
 }
+
+TEST_P(TestPower, execute_w_int)
+{
+
+  std::array<int32_t, 10 * 5 * 3> output;
+  std::array<int32_t, 10 * 5 * 3> input;
+  std::array<int32_t, 10 * 5 * 3> valid;
+
+  std::fill(input.begin(), input.end(), static_cast<int32_t>(value));
+  std::fill(valid.begin(), valid.end(), static_cast<int32_t>(std::pow(value, exp)));
+
+  auto gpu_input = cle::Array::create(10, 5, 3, 3, cle::dType::INT32, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(input.data());
+
+  auto gpu_output = cle::tier1::power_func(device, gpu_input, nullptr, exp);
+
+  gpu_output->readTo(output.data());
+  for (int i = 0; i < output.size(); i++)
+  {
+    EXPECT_EQ(output[i], valid[i]);
+  }
+}
+
 INSTANTIATE_TEST_SUITE_P(InstantiationName, TestPower, ::testing::ValuesIn(getParameters()));
