84a2e2e4aae26c2c2600ade119191269ee53295c
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2026-02-27 17:40:20 +0100

    add back copyTo and image copy

--- clic/src/tier1/copy.cpp
+++ clic/src/tier1/copy.cpp
@@ -17,57 +17,48 @@ namespace cle::tier1
 
 namespace kernel
 {
-//   const char * copy = R"CLC(
-// __constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
+  const char * copy = R"CLC(
+__constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
 
-// __kernel void copy(
-//     IMAGE_src_TYPE  src,
-//     IMAGE_dst_TYPE  dst
-// ) 
-// {
-//   const int x = get_global_id(0);
-//   const int y = get_global_id(1);
-//   const int z = get_global_id(2);
+__kernel void copy(
+    IMAGE_src_TYPE  src,
+    IMAGE_dst_TYPE  dst
+) 
+{
+  const int x = get_global_id(0);
+  const int y = get_global_id(1);
+  const int z = get_global_id(2);
 
-//   const IMAGE_src_PIXEL_TYPE value = READ_IMAGE(src, sampler, POS_src_INSTANCE(x,y,z,0)).x;
-//   WRITE_IMAGE(dst, POS_dst_INSTANCE(x,y,z,0), CONVERT_dst_PIXEL_TYPE(value));
-// })CLC";
-
-// std::string copy = R"CLC(
-// __kernel void KERNEL_NAME(__global const INDTYPE* restrict src,
-//                     __global OUTDTYPE* restrict dst,
-//                     const uint n)
-// {
-//     const uint gid = get_global_id(0);
-//     if (gid < n)
-//         dst[gid] = CONVERT(src[gid]);
-// })CLC";
+  const IMAGE_src_PIXEL_TYPE value = READ_IMAGE(src, sampler, POS_src_INSTANCE(x,y,z,0)).x;
+  WRITE_IMAGE(dst, POS_dst_INSTANCE(x,y,z,0), CONVERT_dst_PIXEL_TYPE(value));
+})CLC";
 
 } // namespace kernel
-
-// auto
-// copy_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst) -> Array::Pointer
-// {
-//   tier0::create_like(src, dst);
-//   const KernelInfo    kernel = { "copy", kernel::copy };
-//   const ParameterList params = { { "src", src }, { "dst", dst } };
-//   const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
-//   execute(device, kernel, params, range);
-//   return dst;
-// }
-
-  // if (src->dtype() == dst->dtype())
-  // {
-  //   src->copyTo(dst);
-  //   return dst;
-  // }
 
 auto
 copy_func(const Device::Pointer & device, const Array::Pointer & src, Array::Pointer dst) -> Array::Pointer
 {
   tier0::create_like(src, dst);
 
+  if (src->dtype() == dst->dtype())
+  {
+    // use built-in copy if data types are the same
+    src->copyTo(dst);
+    return dst;
+  }
 
+  if (src->mtype() == mType::IMAGE || dst->mtype() == mType::IMAGE)
+  {
+    // use image copy kernel if either src or dst is an image
+    tier0::create_like(src, dst);
+    const KernelInfo    kernel = { "copy", kernel::copy };
+    const ParameterList params = { { "src", src }, { "dst", dst } };
+    const RangeArray    range = { dst->width(), dst->height(), dst->depth() };
+    execute(device, kernel, params, range);
+    return dst;    
+  }
+
+  // use basic copy kernel for the rest
 
   auto input_type = toString(src->dtype());
   auto output_type = toString(dst->dtype());
@@ -80,7 +71,7 @@ copy_func(const Device::Pointer & device, const Array::Pointer & src, Array::Poi
   // saveFile("kernel.txt", kernel_source);
 
   const KernelInfo    kernel_info = { kernel_name, kernel_source };
-  const ParameterList params = { { "src", src }, { "dst", dst }, { "n", static_cast<uint>(dst->size()) } };
+  const ParameterList params = { { "src", src }, { "dst", dst }, { "n", static_cast<unsigned int>(dst->size()) } };
   
   auto max_work_group_size = device->getMaximumWorkGroupSize();
   auto global_work_size = dst->size();
