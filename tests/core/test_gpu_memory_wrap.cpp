71bbbfcc238e21966d55f8ec8bb9ee1c8e2f072b
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2026-03-09 16:36:59 +0100

    fix test

--- tests/core/test_gpu_memory_wrap.cpp
+++ tests/core/test_gpu_memory_wrap.cpp
@@ -1,26 +1,28 @@
 #include "cle.hpp"
-#include "gtest/gtest.h"
+#include "test_utils.hpp"
 
-namespace cle
-{
+#include <gtest/gtest.h>
 
-class GPUMemoryWrapTest : public ::testing::Test
+class GPUMemoryWrapTest : public ::testing::TestWithParam<std::string>
 {
 protected:
-  Device::Pointer device;
+  std::string          backend;
+  cle::Device::Pointer device;
 
   virtual void
   SetUp()
   {
-    device = BackendManager::getInstance().getBackend().getDevice("", "gpu");
+    backend = GetParam();
+    cle::BackendManager::getInstance().setBackend(backend);
+    device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
     device->setWaitToFinish(true);
   }
 };
 
-TEST_F(GPUMemoryWrapTest, CreateFromGPUMemoryBuffer)
+TEST_P(GPUMemoryWrapTest, CreateFromGPUMemoryBuffer)
 {
   // Create an array with allocated GPU memory
-  auto original = Array::create(32, 32, 1, 2, dType::FLOAT, mType::BUFFER, device);
+  auto original = cle::Array::create(32, 32, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
   ASSERT_NE(original, nullptr);
   ASSERT_TRUE(original->ownsMemory());
 
@@ -29,7 +31,7 @@ TEST_F(GPUMemoryWrapTest, CreateFromGPUMemoryBuffer)
   ASSERT_NE(gpu_ptr, nullptr);
 
   // Create a new array that wraps the same GPU memory
-  auto wrapped = Array::createFromGPUMemory(32, 32, 1, 2, dType::FLOAT, mType::BUFFER, gpu_ptr, device);
+  auto wrapped = cle::Array::createFromGPUMemory(32, 32, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, gpu_ptr, device);
   ASSERT_NE(wrapped, nullptr);
   ASSERT_FALSE(wrapped->ownsMemory());
 
@@ -37,19 +39,19 @@ TEST_F(GPUMemoryWrapTest, CreateFromGPUMemoryBuffer)
   ASSERT_EQ(wrapped->width(), 32);
   ASSERT_EQ(wrapped->height(), 32);
   ASSERT_EQ(wrapped->depth(), 1);
-  ASSERT_EQ(wrapped->dtype(), dType::FLOAT);
-  ASSERT_EQ(wrapped->mtype(), mType::BUFFER);
+  ASSERT_EQ(wrapped->dtype(), cle::dType::FLOAT);
+  ASSERT_EQ(wrapped->mtype(), cle::mType::BUFFER);
 }
 
-TEST_F(GPUMemoryWrapTest, WrappedArrayPointsToSameMemory)
+TEST_P(GPUMemoryWrapTest, WrappedArrayPointsToSameMemory)
 {
   // Create an array and fill it with data
-  auto original = Array::create(16, 16, 1, 2, dType::FLOAT, mType::BUFFER, device);
+  auto original = cle::Array::create(16, 16, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
   original->fill(3.14f);
 
   // Wrap the GPU memory
   auto gpu_ptr = original->get_ptr();
-  auto wrapped = Array::createFromGPUMemory(16, 16, 1, 2, dType::FLOAT, mType::BUFFER, gpu_ptr, device);
+  auto wrapped = cle::Array::createFromGPUMemory(16, 16, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, gpu_ptr, device);
 
   // Read from wrapped array should get the same data
   std::vector<float> original_data(256);
@@ -66,47 +68,48 @@ TEST_F(GPUMemoryWrapTest, WrappedArrayPointsToSameMemory)
   }
 }
 
-TEST_F(GPUMemoryWrapTest, CreateFromGPUMemoryNullPointerThrows)
+TEST_P(GPUMemoryWrapTest, CreateFromGPUMemoryNullPointerThrows)
 {
   // Creating with null GPU memory pointer should throw
-  EXPECT_THROW(Array::createFromGPUMemory(32, 32, 1, 2, dType::FLOAT, mType::BUFFER, nullptr, device), std::runtime_error);
+  EXPECT_THROW(cle::Array::createFromGPUMemory(32, 32, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, nullptr, device), std::runtime_error);
 }
 
-TEST_F(GPUMemoryWrapTest, CreateFromGPUMemoryNullDeviceThrows)
+TEST_P(GPUMemoryWrapTest, CreateFromGPUMemoryNullDeviceThrows)
 {
   // Creating with null device pointer should throw
-  auto allocated = Array::create(32, 32, 1, 2, dType::FLOAT, mType::BUFFER, device);
+  auto allocated = cle::Array::create(32, 32, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
   auto gpu_ptr = allocated->get_ptr();
 
-  EXPECT_THROW(Array::createFromGPUMemory(32, 32, 1, 2, dType::FLOAT, mType::BUFFER, gpu_ptr, nullptr), std::runtime_error);
+  EXPECT_THROW(cle::Array::createFromGPUMemory(32, 32, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, gpu_ptr, nullptr), std::runtime_error);
 }
 
-TEST_F(GPUMemoryWrapTest, MemoryOwnershipTracking)
+TEST_P(GPUMemoryWrapTest, MemoryOwnershipTracking)
 {
   // Array created normally should own memory
-  auto owned = Array::create(16, 16, 1, 2, dType::FLOAT, mType::BUFFER, device);
+  auto owned = cle::Array::create(16, 16, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, device);
   EXPECT_TRUE(owned->ownsMemory());
 
   // Array wrapped from external memory should not own it
   auto gpu_ptr = owned->get_ptr();
-  auto wrapped = Array::createFromGPUMemory(16, 16, 1, 2, dType::FLOAT, mType::BUFFER, gpu_ptr, device);
+  auto wrapped = cle::Array::createFromGPUMemory(16, 16, 1, 2, cle::dType::FLOAT, cle::mType::BUFFER, gpu_ptr, device);
   EXPECT_FALSE(wrapped->ownsMemory());
 }
 
-TEST_F(GPUMemoryWrapTest, CreateFromGPUMemoryDifferentDataTypes)
+TEST_P(GPUMemoryWrapTest, CreateFromGPUMemoryDifferentDataTypes)
 {
   // Test with different data types
-  std::vector<dType> types = { dType::INT8, dType::UINT8, dType::INT16, dType::UINT16, dType::INT32, dType::UINT32, dType::FLOAT };
+  std::vector<cle::dType> types = { cle::dType::INT8, cle::dType::UINT8, cle::dType::INT16, cle::dType::UINT16, cle::dType::INT32, cle::dType::UINT32, cle::dType::FLOAT };
 
   for (auto dtype : types)
   {
-    auto original = Array::create(8, 8, 1, 2, dtype, mType::BUFFER, device);
+    auto original = cle::Array::create(8, 8, 1, 2, dtype, cle::mType::BUFFER, device);
     auto gpu_ptr = original->get_ptr();
-    auto wrapped = Array::createFromGPUMemory(8, 8, 1, 2, dtype, mType::BUFFER, gpu_ptr, device);
+    auto wrapped = cle::Array::createFromGPUMemory(8, 8, 1, 2, dtype, cle::mType::BUFFER, gpu_ptr, device);
 
     EXPECT_EQ(wrapped->dtype(), dtype);
     EXPECT_FALSE(wrapped->ownsMemory());
   }
 }
 
-} // namespace cle
+INSTANTIATE_TEST_SUITE_P(InstantiationName, GPUMemoryWrapTest, ::testing::ValuesIn(getParameters()));
+
