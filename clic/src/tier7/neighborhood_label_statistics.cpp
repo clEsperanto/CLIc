290b54f54426123c3e3c63e6367ce450497f110b
Stephane Rigaud <stephane.rigaud@imba.oeaw.ac.at>
2026-06-18 17:17:31 +0200

    update parametric map usage

--- clic/src/tier7/neighborhood_label_statistics.cpp
+++ clic/src/tier7/neighborhood_label_statistics.cpp
@@ -19,17 +19,33 @@ labels_neighbors_statistics_func(const Device::Pointer &  device,
                                  const Array::Pointer     label,
                                  const std::vector<int> & proximal_distances,
                                  const std::vector<int> & nearest_neighbor_ns,
-                                 const std::vector<int> & dilation_radii) -> StatisticsMap
+                                 const std::vector<int> & dilation_radii,
+                                const bool & include_background) -> StatisticsMap
 {
   auto nei_stats = compute_neighbors_statistics_per_labels(device, label, proximal_distances, nearest_neighbor_ns, dilation_radii);
 
-  // remove background label statistics (index 0) from all exported vectors
-  for (auto & entry : nei_stats)
+  if (!include_background)
   {
-    auto & vec = entry.second;
-    if (!vec.empty())
+    // remove background label statistics (index 0) from all exported vectors
+    for (auto & entry : nei_stats)
     {
-      vec.erase(vec.begin());
+      auto & vec = entry.second;
+      if (!vec.empty())
+      {
+        vec.erase(vec.begin());
+      }
+    }
+  }
+  else
+  {
+    // set background label statistics (index 0) to 0 for all exported vectors
+    for (auto & entry : nei_stats)
+    {
+      auto & vec = entry.second;
+      if (!vec.empty())
+      {
+        vec[0] = 0.0f;
+      }
     }
   }
 
@@ -43,7 +59,7 @@ statistics_of_labelled_neighbors_func(const Device::Pointer &  device,
                                       const std::vector<int> & nearest_neighbor_ns,
                                       const std::vector<int> & dilation_radii) -> StatisticsMap
 {
-  return labels_neighbors_statistics_func(device, label, proximal_distances, nearest_neighbor_ns, dilation_radii);
+  return labels_neighbors_statistics_func(device, label, proximal_distances, nearest_neighbor_ns, dilation_radii, false);
 }
 
 
