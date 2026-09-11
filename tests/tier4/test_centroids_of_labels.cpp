31513c68c4c08928cf0d948e4def11b39ee023d8
Stephane Rigaud <stephane.rigaud@imba.oeaw.ac.at>
2026-09-11 16:56:49 +0200

    rework centroids and test

--- tests/tier4/test_centroids_of_labels.cpp
+++ tests/tier4/test_centroids_of_labels.cpp
@@ -14,7 +14,6 @@ protected:
 
 TEST_P(TestCentroidsOfLabels, execute2d)
 {
-
   std::string param = GetParam();
   cle::BackendManager::getInstance().setBackend(param);
   auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
@@ -32,4 +31,54 @@ TEST_P(TestCentroidsOfLabels, execute2d)
     EXPECT_EQ(output[i], valid[i]);
   }
 }
+
+TEST_P(TestCentroidsOfLabels, allBackgroundWithoutBackgroundCentroid)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
+
+  std::array<uint32_t, 6 * 5 * 1> all_background{};
+  auto gpu_input = cle::Array::create(6, 5, 1, 2, cle::dType::LABEL, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(all_background.data());
+
+  auto gpu_output = cle::tier4::centroids_of_labels_func(device, gpu_input, nullptr, false);
+
+  EXPECT_EQ(gpu_output->width(), 1);
+  EXPECT_EQ(gpu_output->height(), 3);
+  std::array<float, 3> output{};
+  gpu_output->readTo(output.data());
+  EXPECT_EQ(output, (std::array<float, 3>{ 0, 0, 0 }));
+}
+
+TEST_P(TestCentroidsOfLabels, rejectsNegativeOnlyLabels)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
+
+  std::array<int32_t, 6 * 5 * 1> negative_labels;
+  negative_labels.fill(-1);
+  auto gpu_input = cle::Array::create(6, 5, 1, 2, cle::dType::INT32, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(negative_labels.data());
+
+  EXPECT_THROW(cle::tier4::centroids_of_labels_func(device, gpu_input, nullptr, false), std::runtime_error);
+}
+
+TEST_P(TestCentroidsOfLabels, rejectsOutputWithDepth)
+{
+  std::string param = GetParam();
+  cle::BackendManager::getInstance().setBackend(param);
+  auto device = cle::BackendManager::getInstance().getBackend().getDevice("", "gpu");
+  device->setWaitToFinish(true);
+
+  auto gpu_input = cle::Array::create(6, 5, 1, 2, cle::dType::LABEL, cle::mType::BUFFER, device);
+  gpu_input->writeFrom(input.data());
+  auto gpu_output = cle::Array::create(7, 3, 2, 3, cle::dType::FLOAT, cle::mType::BUFFER, device);
+
+  EXPECT_THROW(cle::tier4::centroids_of_labels_func(device, gpu_input, gpu_output, true), std::runtime_error);
+}
+
 INSTANTIATE_TEST_SUITE_P(InstantiationName, TestCentroidsOfLabels, ::testing::ValuesIn(getParameters()));
