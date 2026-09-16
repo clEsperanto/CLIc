848d376c1998a1fface32e2f78658a2c4debcf74
Stephane Rigaud <stephane.rigaud@imba.oeaw.ac.at>
2026-09-16 15:26:23 +0200

    move fft to fft namespace

--- tests/tier8/test_fft.cpp
+++ tests/tier8/test_fft.cpp
@@ -133,7 +133,7 @@ TEST_P(TestFFT, executeConvolutionFunction)
   auto gpu_psf = cle::Array::create(2, 1, 1, 1, cle::dType::FLOAT, cle::mType::BUFFER, device);
   gpu_psf->writeFrom(kernel.data());
 
-  auto gpu_final = cle::tier8::convolve_fft_func(device, gpu_input, gpu_psf, nullptr, false);
+  auto gpu_final = cle::fft::convolve_func(device, gpu_input, gpu_psf, nullptr, false);
 
   std::vector<float> output(gpu_final->size());
   gpu_final->readTo(output.data());
@@ -164,7 +164,7 @@ TEST_P(TestFFT, executeDeconvolutionFunction)
   auto gpu_psf = cle::Array::create(gpu_input);
   gpu_psf->writeFrom(kernel.data());
 
-  auto gpu_output = cle::tier8::deconvolve_fft_func(device, gpu_input, gpu_psf, nullptr, nullptr, 100, 0);
+  auto gpu_output = cle::fft::deconvolve_func(device, gpu_input, gpu_psf, nullptr, nullptr, 100, 0);
 
   std::vector<float> output(gpu_output->size());
   gpu_output->readTo(output.data());
